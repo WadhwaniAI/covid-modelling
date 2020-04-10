96837d478090a039e88d8febdf115d706798a1a6
chinandy1991 <chinandy1991@gmail.com>
2020-04-10 13:35:09 -0400

    fix over float

--- notebooks/opt/opt_gaboh_single_int.py
+++ notebooks/opt/opt_gaboh_single_int.py
@@ -52,37 +52,43 @@ def calculate_opt(intervention):
     H = states_int_array[5]
     F = states_int_array[8]
     
-    S_coeficeint=1
-    E_coeficeint=0.825
-    I_coeficeint=0.75
-    R_mild_coeficeint=0.625
-    R_severe_coeficeint=0.5
-    R_severe_home_coeficeint=0.325
-    R_R_fatal_coeficeint=0.25
-    C_coeficeint=1
-    D_coeficeint=0
-    
     # S_coeficeint=1
-    # E_coeficeint=0
-    # I_coeficeint=0.5
-    # R_mild_coeficeint=0
-    # R_severe_coeficeint=0
-    # R_severe_home_coeficeint=0
-    # R_R_fatal_coeficeint=0
+    # E_coeficeint=0.825
+    # I_coeficeint=0.75
+    # R_mild_coeficeint=0.625
+    # R_severe_coeficeint=0.5
+    # R_severe_home_coeficeint=0.325
+    # R_R_fatal_coeficeint=0.25
     # C_coeficeint=1
     # D_coeficeint=0
     
+    S_coeficeint=1
+    E_coeficeint=0
+    I_coeficeint=0
+    R_mild_coeficeint=0
+    R_severe_coeficeint=0
+    R_severe_home_coeficeint=0
+    R_R_fatal_coeficeint=0
+    C_coeficeint=1
+    D_coeficeint=0
+    # plt.plot(range(len(states_int_array[0])),states_int_array[0])
+    # plt.plot(range(len(states_int_array[0])),states_int_array[2])
+    # plt.plot(range(len(states_int_array[0])),states_int_array[7])
+    # plt.plot(range(len(states_int_array[0])),states_int_array[8])
     
     coeficeint=[S_coeficeint,E_coeficeint,I_coeficeint,R_mild_coeficeint,R_severe_coeficeint,R_severe_home_coeficeint,R_severe_home_coeficeint,R_R_fatal_coeficeint,C_coeficeint,D_coeficeint]
     
     # objective = N*intervention_duration*0.8 + 1e5 + F[-1]*1e4 + np.sum(H)
     objective=0
     for i in range(8):
-        objective+=-coeficeint[i]*np.sum(states_int_array[i])
-    return(objective/1e8)
+        objective+=-coeficeint[i]*np.sum(states_int_array[i]/1e8)
+        # print(np.sum(states_int_array[i]/1e8))
+    return(objective)
+    
+# print(calculate_opt([50, 100, 1.5]))
 
-res = opt.minimize(calculate_opt, [50,100,3], method='nelder-mead',options={'xatol': 1e0, 'disp': True, 'fatol': 1e-2, 'maxiter': 1e4, 'maxfev': 1e4})
-print(res)
+# res = opt.minimize(calculate_opt, [50,100,1], method='nelder-mead',options={'xatol': 1e0, 'disp': True, 'fatol': 1e-2, 'maxiter': 1e4, 'maxfev': 1e4})
+# print(res)
 # res = opt.minimize(calculate_opt, [50,100,3], method='Powell',options={'xtol': 1e0, 'disp': True, 'ftol': 1e-2, 'maxiter': 1e4, 'maxfev': 1e4})
 
 # obj = np.zeros(300)
@@ -96,10 +102,14 @@ print(res)
 # plt.xlabel('intervention duration')
 # plt.legend()
 # plt.show()
+
+i1=55
+i2=100
+i3=1.2
 r=10
 
 obj_S = np.zeros((2*r,2*r))
-for i in range(50-r,50+r):
-    for j in range(105-r,105+r):
-        obj_S[i-(50-r),j-(105-r)]=-calculate_opt([i, j, 3.18160778])
+for i in range(i1-r,i1+r):
+    for j in range(i2-r,i2+r):
+        obj_S[i-(i1-r),j-(i2-r)]=-calculate_opt([i, j, i3])
 ax = sns.heatmap(obj_S)
