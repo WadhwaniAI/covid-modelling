203e97d9ab9c923d1e0a6e50343ea48012baa7d7
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-04-24 21:34:09 +0530

    [TESTING REQUIRED] revamp of optimisation code

--- main/losses.py
+++ main/losses.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 class Loss_Calculator():
 
-    def _calc_rmse(y_pred, y_true, log=True):
+    def _calc_rmse(self, y_pred, y_true, log=False):
         if log:
             y_true = np.log(y_true)
             y_pred = np.log(y_pred)
         loss = np.sqrt(np.mean((y_true - y_pred)**2))
         return loss
 
-    def _calc_mape(y_pred, y_true):
+    def _calc_mape(self, y_pred, y_true):
         y_pred = y_pred[y_true > 0]
         y_true = y_true[y_true > 0]
 
@@ -18,35 +18,29 @@ class Loss_Calculator():
         loss = np.mean(ape)
         return loss
 
-    def calc_loss_dict(states_time_matrix, df, method='rmse', rmse_log=False):
-        pred_hospitalisations = states_time_matrix[6] + states_time_matrix[7] + states_time_matrix[8]
-        pred_recoveries = states_time_matrix[9]
-        pred_fatalities = states_time_matrix[10]
-        pred_infectious_unknown = states_time_matrix[2] + states_time_matrix[4]
-        pred_total_cases = pred_hospitalisations + pred_recoveries + pred_fatalities
-        
+    def calc_loss_dict(self, df_prediction, df_true, method='rmse'):
         if method == 'rmse':
-            if rmse_log:
-                calculate = lambda x, y : self._calc_rmse(x, y)
-            else:
-                calculate = lambda x, y : self._calc_rmse(x, y, log=False)
-        
+            calculate = lambda x, y : self._calc_rmse(x, y)
+        if method == 'rmse_log':
+            calculate = lambda x, y : self._calc_rmse(x, y, log=True)
         if method == 'mape':
-                calculate = lambda x, y : self._calc_mape(x, y)
+            calculate = lambda x, y : self._calc_mape(x, y)
         
         losses = {}
-        losses['hospitalised'] = calculate(pred_hospitalisations, df['Hospitalised'])
-        losses['recovered'] = calculate(pred_recoveries, df['Recovered'])
-        losses['fatalities'] = calculate(pred_fatalities, df['Fatalities'])
-        losses['active_infections'] = calculate(pred_infectious_unknown, df['Unknown Active Infections'])
-        losses['total'] = calculate(pred_total_cases, df['Total Infected'])
-        
+        for compartment in ['hospitalised', 'recovered', 'fatalities', 'infectious_unknown', 'total_infected']:
+            try:
+                losses[compartment] = calculate(df_prediction[compartment], df_true[compartment])
+            except Exception:
+                continue
         return losses
 
 
-    def calc_loss(states_time_matrix, df, which_compartments=['hospitalised', 'recovered', 'total', 'fatalities'], method='rmse', rmse_log=False):
-        losses = self.calc_loss_dict(states_time_matrix, df, method, rmse_log)
+    def calc_loss(self, states_time_matrix, df, which_compartments=['hospitalised', 'recovered', 'total', 'fatalities'], method='rmse'):
+        losses = self.calc_loss_dict(states_time_matrix, df, method)
         loss = 0
         for compartment in which_compartments:
-            loss += losses[compartment]
+            try:
+                loss += losses[compartment]
+            except Exception:
+                continue
         return loss
--- main/optimiser.py
+++ main/optimiser.py
@@ -1,5 +1,21 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+from hyperopt import hp, tpe, fmin, Trials
+from tqdm import tqdm
+from tqdm.notebook import tqdm
+
+from collections import OrderedDict
+import itertools
+from functools import partial
+import datetime
+from joblib import Parallel, delayed
+
+from models.seir.seir_testing import SEIR_Testing
+from utils.plotting import create_plots
+from utils.generic import init_params
 from main.losses import Loss_Calculator
 
 class Optimiser():
@@ -7,24 +23,51 @@ class Optimiser():
     def __init__(self):
         self.loss_calculator = Loss_Calculator()
 
-    def solve_and_compute_loss(variable_params, default_params, df_total, total_days, train_indices=[-20, -10], loss_method='rmse'):
+    def solve(self, variable_params, default_params, df_true, end_date=None):
         params_dict = {**variable_params, **default_params}
-        vanilla_params, testing_params, state_init_values = init_params(**params_dict)
-        solver = SEIR_Testing(vanilla_params, testing_params, state_init_values)
+        solver = SEIR_Testing(**params_dict)
+        if end_date == None:
+            end_date = df_true.iloc[-1, :]['date']
+        else:
+            end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        total_days = (params_dict['starting_date'] - end_date).days,
         sol = solver.solve_ode(total_no_of_days=total_days - 1, time_step=1, method='Radau')
-        states_time_matrix = (sol.y*vanilla_params['N']).astype('int')
+        df_prediction = solver.return_predictions(sol)
+        return df_prediction
 
-        loss = self.loss_calculator.calc_loss(states_time_matrix[:, train_indices[0]:train_indices[1]], \ 
-        df.iloc[train_indices[0]:train_indices[1], :], loss_method)
+
+    # TODO add cross validation support
+    def solve_and_compute_loss(self, variable_params, default_params, df_true, total_days, 
+                               which_compartments=['hospitalised', 'recovered', 'total', 'fatalities'], 
+                               loss_indices=[-20, -10], loss_method='rmse', return_dict=False):
+        params_dict = {**variable_params, **default_params}
+        solver = SEIR_Testing(**params_dict)
+        sol = solver.solve_ode(total_no_of_days=total_days - 1, time_step=1, method='Radau')
+        df_prediction = solver.return_predictions(sol)
+
+        # Choose which indices to calculate loss on
+        # TODO Add slicing capabilities on the basis of date
+        if loss_indices == None:
+            df_prediction_slice = df_prediction.iloc[:, :]
+            df_true_slice = df_true.iloc[:, :]
+        else:
+            df_prediction_slice = df_prediction.iloc[loss_indices[0]:loss_indices[1], :]
+            df_true_slice = df_true.iloc[loss_indices[0]:loss_indices[1], :]
+        if return_dict:
+            loss = self.loss_calculator.calc_loss_dict(df_prediction_slice, df_true_slice, method=loss_method)
+        else:
+            loss = self.loss_calculator.calc_loss(df_prediction_slice, df_true_slice, 
+                                                  which_compartments=which_compartments, method=loss_method)
         return loss
 
-    def _create_dict(param_names, values):
+    def _create_dict(self, param_names, values):
         params_dict = {param_names[i]: values[i] for i in range(len(values))}
         return params_dict
 
-    def init_optimiser(df_true, N=1e7, lockdown_date='2020-03-25', lockdown_removal_date='2020-05-03', T_hosp = 0.001, P_fatal = 0.01):
-        init_infected = max(df_true.loc[0, 'Total Infected'], 1)
-        start_date = df_true.loc[0, 'Date']
+    def init_default_params(self, df_true, N=1e7, lockdown_date='2020-03-25', lockdown_removal_date='2020-05-03', 
+                            T_hosp=0.001, P_fatal=0.01):
+        init_infected = max(df_true.iloc[0, :]['total_infected'], 1)
+        start_date = df_true.iloc[0, :]['date']
         intervention_date = datetime.datetime.strptime(lockdown_date, '%Y-%m-%d')
         lockdown_removal_date = datetime.datetime.strptime(lockdown_removal_date, '%Y-%m-%d')
 
@@ -32,16 +75,16 @@ class Optimiser():
             'N' : N,
             'init_infected' : init_infected,
             'intervention_day' : (intervention_date - start_date).days,
-            'lockdown_removal_day' : (lockdown_removal_date - start_date).days,
+            'intervention_removal_day': (lockdown_removal_date - start_date).days,
             'T_hosp' : T_hosp,
-            'P_fatal' : P_fatal
+            'P_fatal' : P_fatal,
+            'starting_date' : start_date
         }
         return default_params
 
-    def gridsearch(df_true, default_params, variable_param_ranges, method='rmse'):
-        
-        total_days = len(df_true['Date'])
-
+    def gridsearch(self, df_true, default_params, variable_param_ranges, method='rmse', loss_indices=[-20, -10], debug=False):
+        """
+        What variable_param_ranges is supposed to look like
         variable_param_ranges = {
             'R0' : np.linspace(1.8, 3, 13),
             'T_inc' : np.linspace(3, 5, 5),
@@ -50,21 +93,30 @@ class Optimiser():
             'P_severe' : np.linspace(0.3, 0.9, 25),
             'intervention_amount' : np.linspace(0.4, 1, 31)
         }
-        
+        """
+        total_days = len(df_true['date'])
+
         rangelists = list(variable_param_ranges.values())
         cartesian_product_tuples = itertools.product(*rangelists)
         list_of_param_dicts = [self._create_dict(list(variable_param_ranges.keys()), values) for values in cartesian_product_tuples]
 
-        partial_solve_and_compute_loss = partial(solve_and_compute_loss, default_params = default_params, df=df_true_fitting, 
-                                                 total_days=total_days, method = method) 
+        partial_solve_and_compute_loss = partial(self.solve_and_compute_loss, default_params=default_params, df_true=df_true,
+                                                 total_days=total_days, loss_method=method, loss_indices=loss_indices)
         
-        loss_array = Parallel(n_jobs=40)(delayed(partial_solve_and_compute_loss)(params_dict) for params_dict in tqdm(list_of_param_dicts))
+        # If debugging is enabled the gridsearch is not parallelised
+        if debug:
+            loss_array = []
+            for params_dict in tqdm(list_of_param_dicts):
+                loss_array.append(partial_solve_and_compute_loss(params_dict))
+        else:
+            loss_array = Parallel(n_jobs=40)(delayed(partial_solve_and_compute_loss)(params_dict) for params_dict in tqdm(list_of_param_dicts))
                     
-        return loss_array, list_of_param_dicts, df_true
+        return loss_array, list_of_param_dicts
 
-    def bayes_opt(df_true, default_params, variable_param_ranges, method='rmse', num_evals=3500):
-        
-        variable_params = {
+    def bayes_opt(self, df_true, default_params, variable_param_ranges, method='rmse', num_evals=3500, loss_indices=[-20, -10]):
+        """
+        What variable_param_ramges is supposed to look like : 
+        variable_param_ranges = {
             'R0' : hp.uniform('R0', 1.6, 3),
             'T_inc' : hp.uniform('T_inc', 4, 5),
             'T_inf' : hp.uniform('T_inf', 3, 4),
@@ -72,9 +124,11 @@ class Optimiser():
             'P_severe' : hp.uniform('P_severe', 0.3, 0.99),
             'intervention_amount' : hp.uniform('intervention_amount', 0.3, 1)
         }
+        """
+        total_days = len(df_true['date'])
         
-        partial_solve_and_compute_loss = partial(solve_and_compute_loss, default_params = default_params, df=df_true_fitting, 
-                                                 total_days=total_days, method = method) 
+        partial_solve_and_compute_loss = partial(self.solve_and_compute_loss, default_params=default_params, df_true=df_true,
+                                                 total_days=total_days, loss_method=method, loss_indices=loss_indices)
         
         searchspace = variable_param_ranges
         
@@ -87,7 +141,7 @@ class Optimiser():
         
         return best, trials
 
-    def evaluate_losses(best, default_params, df_train, df_val):
+    def evaluate_losses(self, best, default_params, df_train, df_val):
         start_date = df_train.iloc[0, 0]
         simulate_till = df_val.iloc[-1, 0]
         total_no_of_days = (simulate_till - start_date).days + 1
@@ -103,10 +157,12 @@ class Optimiser():
         train_output = states_time_matrix[:, :no_of_train_days]
         val_output = states_time_matrix[:, -no_of_val_days:]
         
-        rmse_loss = calc_loss_dict(train_output, df_train, method='rmse')
+        rmse_loss = self.loss_calculator.calc_loss_dict(
+            train_output, df_train, method='rmse')
         rmse_loss = pd.DataFrame.from_dict(rmse_loss, orient='index', columns=['rmse'])
         
-        mape_loss = calc_loss_dict(train_output, df_train, method='mape')
+        mape_loss = self.loss_calculator.calc_loss_dict(
+            train_output, df_train, method='mape')
         mape_loss = pd.DataFrame.from_dict(mape_loss, orient='index', columns=['mape'])
         
         train_losses = pd.concat([rmse_loss, mape_loss], axis=1)
@@ -117,12 +173,14 @@ class Optimiser():
         pred_infectious_unknown = val_output[2] + val_output[4]
         pred_total_cases = pred_hospitalisations + pred_recoveries + pred_fatalities
         print('Pred', pred_hospitalisations, pred_total_cases)
-        print('True', df_val['Hospitalised'].to_numpy(), df_val['Total Infected'].to_numpy())
+        print('True', df_val['hospitalised'].to_numpy(), df_val['total_infected'].to_numpy())
         
-        rmse_loss = calc_loss_dict(val_output, df_val, method='rmse')
+        rmse_loss = self.loss_calculator.calc_loss_dict(
+            val_output, df_val, method='rmse')
         rmse_loss = pd.DataFrame.from_dict(rmse_loss, orient='index', columns=['rmse'])
         
-        mape_loss = calc_loss_dict(val_output, df_val, method='mape')
+        mape_loss = self.loss_calculator.calc_loss_dict(
+            val_output, df_val, method='mape')
         mape_loss = pd.DataFrame.from_dict(mape_loss, orient='index', columns=['mape'])
         
         val_losses = pd.concat([rmse_loss, mape_loss], axis=1)
