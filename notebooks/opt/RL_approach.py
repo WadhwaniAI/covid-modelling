c2f74f582871b70c1b3c5416f67ca75139a0e8ee
chinandy1991 <chinandy1991@gmail.com>
2020-04-26 20:45:58 -0400

    RL approach

--- notebooks/opt/RL_approach.py
+++ notebooks/opt/RL_approach.py
@@ -1,108 +1,229 @@
-import numpy as np
-from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-import datetime
-import pandas as pd
 import sys
 import os
 import math
-sys.path.append('../..')
-import scipy.optimize as opt
-from models.optim.SEIR_Discrete import SEIR_Discrete
+import numpy as np
+# sys.path.append('../..')
+# from models.optim.SEIR_Discrete import SEIR_Discrete
+from SEIR_Discrete import SEIR_Discrete
+from sklearn.ensemble import ExtraTreesRegressor
+import pickle
+import time
+from tqdm import tqdm, trange
 
+def is_fitted(sklearn_regressor):
+    """Helper function to determine if a regression model from scikit-learn has
+    ever been `fit`"""
+    return hasattr(sklearn_regressor, 'n_outputs_')
 
+"""Helper class to run Fitted Q Iteration on the SEIR_Discrete task."""
+class FittedQIteration(object):
+    def __init__(self, regressor=None):
+        """Initialize simulator and regressor. Can optionally pass a custom
+        `regressor` model (which must implement `fit` and `predict` -- you can
+        use this to try different models like linear regression or NNs)"""
+        self.simulator = SEIR_Discrete()
+        self.regressor = regressor or ExtraTreesRegressor()
 
-class Environment:
-    def __init__(self, graph):
-        self.simulator = SEIR_Env
-
-    def random_action(self):
-        action = random.sample(self.simulator.possible_nodes,int(min(self.simulator.n,self.simulator.budget)))
-        return action
-
-    def policy(self, state, observation, eps=0.1):
-        if np.random.rand() < eps:
-            return self.random_action()
+    def Q(self, states):
+        """Return the Q function estimate of `states` for each action"""
+        if not is_fitted(self.regressor):
+            # If not fitted, return 0s in the right shape
+            return np.zeros((len(states), self.simulator.num_actions))
         else:
-            return self.greedy_action(state,observation) ###   
+            # Otherwise use the trained regression model
+            return np.array([self.regressor.predict(self.encode(states, action))
+                             for action in range(self.simulator.num_actions)]).T
 
-    def calc_pseudo_reward(self,state):
-        reward=self.simulator.n-sum(state)
-        return reward
+    def policy(self, state, eps=0.1):
+        """Return the epsilon-greedy action based on the current policy (or a
+        random action if the Q function hasn't yet been estimated."""
+        if np.random.rand() < eps or not is_fitted(self.regressor):
+            return np.random.choice(self.simulator.num_actions)
+        else:
+            return self.Q([state]).argmax()
 
-    def run_episode(self, eps=0.1, discount=0.98):
-        S, A, R = [], [], [], []
-        cumulative_reward = 0
+    def run_episode(self, eps=0.1, episode_length=200):
+        """Run a single episode on the HIV simulator using the current policy.
+        Can pass a custom `eps` to test out varying levels of randomness.
+        Return the states visited, actions taken, and rewards received."""
+        S = np.zeros((episode_length+1, self.simulator.num_states))
+        A = np.zeros(episode_length)
+        R = np.zeros(episode_length)
+
         self.simulator.reset()
-        observation = self.simulator.observe()
-        state = 0.5 * np.ones(self.simulator.n)
-        O.append(observation)
-        for t in range(self.simulator.T):
-            state = self.belief_state(observation, state)
-            S.append(state)
-            action = self.policy(state, observation, eps)
-            self.simulator.perform(active_screen_list=action)#Transition Happen
-            state_ = self.belief_transition(observation, action, state) #S'
-            reward = self.calc_pseudo_reward(state)
-            observation = self.simulator.observe()
-            state=state_
-            A.append(action)
-            R.append(reward)
-            S.append(state_)
-            cumulative_reward += reward * (discount**t)
-        return S, A, R, cumulative_reward
+        S[0] = self.simulator.STATE
 
-    
-    def run_episode_real(self, eps=0.1, discount=0.98):
-        O, S, A, R = [], [], [], []
-        cumulative_reward = 0
-        self.simulator.reset()
-        observation = self.simulator.observe()
-        state = 0.5 * np.ones(self.simulator.n)
-        O.append(observation)
-        for t in range(self.simulator.T):
-            state = self.belief_state(observation, state)
-            S.append(state)
-            action = self.policy(state, observation, eps)
-            self.simulator.perform(active_screen_list=action)#Transition Happen
-            reward = self.simulator.calc_reward()
-            state = self.belief_transition(observation, action, state) #S'
-            observation = self.simulator.observe()
-            A.append(action)
-            R.append(reward)
-            O.append(observation)
-            S.append(state)
-            cumulative_reward += reward * (discount**t)
-        return O, S, A, R, cumulative_reward
+        for t in range(episode_length):
+            A[t] = self.policy(S[t], eps=eps)
+            R[t], S[t+1] = self.simulator.perform(A[t])
+        return S, A, R
+
+    def fit_Q(self, episodes, num_iters=100, discount=0.98):
+        """Fit and re-fit the Q function using historical data for the
+        specified number of `iters` at the specified `discount` factor"""
+        S1 = np.vstack([ep[0][:-1] for ep in episodes])
+        S2 = np.vstack([ep[0][1:] for ep in episodes])
+        A = np.hstack([ep[1] for ep in episodes])
+        R = np.hstack([ep[2] for ep in episodes])
+        inputs = self.encode(S1, A)
+        progress = tqdm(range(num_iters), file=sys.stdout,desc='num_iters')
+        for _ in range(num_iters):
+            sys.stdout.flush()
+            progress.update()
+            progress.refresh()
+            targets = R + discount * self.Q(S2).max(axis=1)
+            self.regressor.fit(inputs, targets)
+
+    def fit(self, num_refits=10, num_episodes=15, save=False):
+        """Perform fitted-Q iteration. For `outer_iters` steps, gain
+        `num_episodes` episodes worth of experience using the current policy
+        (which is initially random), then fit or re-fit the Q-function. Return
+        the full set of episode data."""
+        episodes = []
+        progress = tqdm(range(num_refits), file=sys.stdout,desc='num_refits')
+        for i in range(num_refits):
+            progress.update(1)
+            for _ in range(num_episodes):
+                episodes.append(self.run_episode())
+            self.fit_Q(episodes)
+
+            if save:
+                with open('./fqi-regressor-iter-{}.pkl'.format(i+1), 'wb') as f:
+                    pickle.dump(self.regressor, f)
+
+        return episodes
+
+    def encode(self, states, actions):
+        """Encode states and actions as a single input array, suitable for
+        passing into the `fit` method of a scikit-learn model."""
+        if isinstance(actions, int):
+            actions = [actions] * len(states)
+        actions = np.array([[a] for a in actions])
+        states = np.array(states)
+        return np.hstack([states, actions])
 
 if __name__ == '__main__':
-    method='FQI'
+    print('Here goes nothing')
+    env=FittedQIteration()
+    episodes=env.fit( num_refits=10, num_episodes=30)
+ 
+    with open('Result_30EP.pickle', 'wb') as f:
+                pickle.dump([env,episodes], f)
+                
+                
+                
+    # with open('Result_30EP.pickle', 'rb') as f:
+    #     X = pickle.load(f)  
+    # env=X[0]
+    # episodes=X[1]
+    
+###################D1##############################
+    
+    discount=0.98
+    real_episodes=env.run_episode(eps=0)
+    reward=real_episodes[1]
+    test_r=0
+    
+    
+    state_t=real_episodes[0][0]
+    state=state_t[2:]
+    
+    estimate_r=env.Q([state])[0]
+    print(state_t[:2])
+    print(estimate_r[2])
+    
+    
+    for i in range(len(reward)):
+        test_r+=reward[i]*(discount**i)
+    all_r=[]
+    for i in range(10): 
+        e_r=0
+        for j in range(30): 
+#            ep=episodes[i*10+j]
+            ep=episodes[i*10+j]
+            reward=ep[1]
+            r=0
+            for e in range(len(reward)):
+                r+=reward[e]*(discount**e)
+            e_r+=r
+        all_r.append(e_r/30)
+#        
+    all_f=[]
+    feature_num=1-1
+    f_t=np.zeros((200,150))
+    for i in range(10): 
+        for j in range(15):
+            ep=episodes[i*10+j]
+            feature=ep[0]
+            for e in range(len(reward)):
+                f_t[e][(15*i+j)]=feature[e][feature_num+2]
 
-    if method == 'DQN':
-        model = DeepQNetworks(graph=g)
-    elif method == 'FQI':
-        model = FittedQIteration(graph=g)
-    else:
-        raise ValueError('Not implemented method')
-    discount=0.9
-    # random policy
-    cumulative_rewards = []
-    for i in range(10):
-        O, S, A, R, cumulative_reward = model.run_episode_real(eps=1, discount=discount)
-        cumulative_rewards.append(cumulative_reward)
-    print('random action reward:', np.mean(cumulative_rewards))
-    eps=0.05
-    if method == 'DQN':
-        model.fit(discount=discount, num_iterations=100, eps= eps, num_epochs=100) # for DQN
-    elif method == 'FQI':
-        model.fit(num_refits=10, num_episodes=10,eps=eps, discount=discount) # for FQI
-    else:
-        raise ValueError('Not implemented method')
+    ff_t=np.zeros(200)
+    for i in range(1): 
+        for j in range(1):
+            ep=real_episodes
+            feature=ep[0]
+            for e in range(len(reward)):
+                
+                ff_t[e]=feature[e][feature_num+2]    
 
-    # optimal policy
-    cumulative_rewards = []
-    for i in range(10):
-        O, S, A, R, cumulative_reward = model.run_episode_real(eps=0, discount=discount)
-        cumulative_rewards.append(cumulative_reward)
-    print('optimal reward:', np.mean(cumulative_rewards))
+###################D2##############################
+#    Eps=1
+#    feature_num=6-1
+#    
+#    real_episodes=[]
+##    for i in range(100):
+##        real_episodes.append(env.run_episode(eps=Eps))
+###        if(i>=50):
+###            real_episodes.append(env.run_episode(eps=Eps))
+##    env2=FittedQIteration()
+##    env2.fit_Q(real_episodes)
+##    with open('hw2Result_encode_'+str(Eps)+'.pickle', 'wb') as f:
+##            pickle.dump(env2, f)
+##            
+#            
+#    with open('hw2Result_encode_'+str(Eps)+'.pickle', 'rb') as f:
+#        env2 = pickle.load(f)
+#    
+#    real_episodes2=env2.run_episode(eps=0)
+#    discount=0.98
+#    reward=real_episodes2[1]
+#    state_t=real_episodes2[0][0]
+#    state=state_t[2:]
+#    test_r=0
+#    estimate_r=env2.Q([state])[0]
+#    print(state_t[:2])
+#    print(estimate_r[2])
+#    for i in range(len(reward)):
+#        test_r+=reward[i]*(discount**i)
+#    print(test_r)
+#    all_r=[]
+#    for i in range(10): 
+#        e_r=0
+#        for j in range(15): 
+#            ep=episodes[i*10+j]
+#            reward=ep[1]
+#            r=0
+#            for e in range(len(reward)):
+#                r+=reward[e]*(discount**e)
+#            e_r+=r
+#        all_r.append(e_r/15)
+##        
+#    all_f=[]
+#    
+#    f_t=np.zeros((200,150))
+#    for i in range(10): 
+#        for j in range(15):
+#            ep=episodes[i*10+j]
+#            feature=ep[0]
+#            for e in range(len(reward)):
+#                f_t[e][(15*i+j)]=feature[e][feature_num+2]
+#
+#    ff_t=np.zeros(200)
+#    for i in range(1): 
+#        for j in range(1):
+#            ep=real_episodes2
+#            feature=ep[0]
+#            for e in range(len(reward)):
+#                
+#                ff_t[e]=feature[e][feature_num+2]    
--- notebooks/opt/SEIR_Discrete.py
+++ notebooks/opt/SEIR_Discrete.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class SEIR_Discrete(object):
     def __init__(self, S=0.99, E=0, I=0.01, R_mild=0, R_severe=0, R_severe_hosp=0, R_fatal=0, C=0, D=0,B=50):
-        self.STATE=[S, E, I, R_mild, R_severe, R_severe_hosp, R_fatal, C, D,B]
+        self.STATE=np.array([S, E, I, R_mild, R_severe, R_severe_hosp, R_fatal, C, D,B])
         self.R0 = 2.2 
         self.T_inf = 2.9
         self.T_trans = self.T_inf/self.R0
@@ -21,8 +21,9 @@ class SEIR_Discrete(object):
         self.t=0
         '''Intervention Relateted'''
         self.num_actions=5
+        self.num_states=10
     def reset(self):
-        self.STATE=[0.99, 0, 0.01, 0, 0, 0, 0, 0, 0,50]
+        self.STATE=np.array([0.99, 0, 0.01, 0, 0, 0, 0, 0, 0,50])
         self.t=0
     
     def is_action_legal(self,ACTION):
@@ -58,24 +59,27 @@ class SEIR_Discrete(object):
         return T_trans_c*self.T_inf/self.R0    
     
     def perform(self,ACTION):
-        if self.is_action_legal(ACTION):
-            STATE_=self.STATE.copy()
+        STATE_=self.STATE.copy()
+        if self.is_action_legal(ACTION)==False:
+            # print("Exceed Budget")
+            r=0
         else:
-            print("Exceed Budget")
-        # 0:S, 1:E, 2:I, 3:R_mild, 4:R_severe, 5:R_severe_hosp, 6:R_fatal, 7:C, 8:D, 9:B
-        T_trans=self.get_action_T(ACTION)
-        STATE_[0] = self.STATE[0]-self.STATE[2]*self.STATE[0]/(T_trans)
-        STATE_[1] = self.STATE[1]+self.STATE[2]*self.STATE[0]/(T_trans) - self.STATE[1]/self.T_inc
-        STATE_[2] = self.STATE[2]+self.STATE[1]/self.T_inc - self.STATE[2]/self.T_inf
-        STATE_[3] = self.STATE[3]+(self.P_mild*self.STATE[2])/self.T_inf - self.STATE[3]/self.T_recov_mild
-        STATE_[4] = self.STATE[4]+(self.P_severe*self.STATE[2])/self.T_inf - self.STATE[4]/self.T_hosp
-        STATE_[5] = self.STATE[5]+self.STATE[4]/self.T_hosp - self.STATE[5]/self.T_recov_severe
-        STATE_[6] = self.STATE[6]+(self.P_fatal*self.STATE[2])/self.T_inf - self.STATE[6]/self.T_death
-        STATE_[7] = self.STATE[7]+self.STATE[3]/self.T_recov_mild + self.STATE[5]/self.T_recov_severe
-        STATE_[8] = self.STATE[8]+self.STATE[6]/self.T_death
-        STATE_[9] = self.STATE[9]-self.get_action_cost(ACTION)
-        self.STATE=STATE_.copy()
-        self.t+=1
+            # 0:S, 1:E, 2:I, 3:R_mild, 4:R_severe, 5:R_severe_hosp, 6:R_fatal, 7:C, 8:D, 9:B
+            T_trans=self.get_action_T(ACTION)
+            STATE_[0] = self.STATE[0]-self.STATE[2]*self.STATE[0]/(T_trans)
+            STATE_[1] = self.STATE[1]+self.STATE[2]*self.STATE[0]/(T_trans) - self.STATE[1]/self.T_inc
+            STATE_[2] = self.STATE[2]+self.STATE[1]/self.T_inc - self.STATE[2]/self.T_inf
+            STATE_[3] = self.STATE[3]+(self.P_mild*self.STATE[2])/self.T_inf - self.STATE[3]/self.T_recov_mild
+            STATE_[4] = self.STATE[4]+(self.P_severe*self.STATE[2])/self.T_inf - self.STATE[4]/self.T_hosp
+            STATE_[5] = self.STATE[5]+self.STATE[4]/self.T_hosp - self.STATE[5]/self.T_recov_severe
+            STATE_[6] = self.STATE[6]+(self.P_fatal*self.STATE[2])/self.T_inf - self.STATE[6]/self.T_death
+            STATE_[7] = self.STATE[7]+self.STATE[3]/self.T_recov_mild + self.STATE[5]/self.T_recov_severe
+            STATE_[8] = self.STATE[8]+self.STATE[6]/self.T_death
+            STATE_[9] = self.STATE[9]-self.get_action_cost(ACTION)
+            self.STATE=STATE_.copy()
+            self.t+=1
+            r=self.calc_reward()
+        return r, self.STATE
 
     def calc_reward(self):
         S_coeficeint=1
@@ -87,7 +91,7 @@ class SEIR_Discrete(object):
         R_R_fatal_coeficeint=0
         C_coeficeint=1
         D_coeficeint=0
-        coeficeint=[S_coeficeint,E_coeficeint,I_coeficeint,R_mild_coeficeint,R_severe_coeficeint,R_severe_home_coeficeint,R_severe_home_coeficeint,R_R_fatal_coeficeint,C_coeficeint,D_coeficeint]
+        coeficeint=np.array([S_coeficeint,E_coeficeint,I_coeficeint,R_mild_coeficeint,R_severe_coeficeint,R_severe_home_coeficeint,R_severe_home_coeficeint,R_R_fatal_coeficeint,C_coeficeint,D_coeficeint])
         reward=sum(coeficeint[:]*self.STATE[:])
         return reward
     
