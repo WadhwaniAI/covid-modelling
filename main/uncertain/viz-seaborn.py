e612f6b71816e9552ae5ec9266ba4993bf3b0930
Jerome White <jerome@wadhwaniai.org>
2020-04-29 13:34:30 +0000

    Use relative days

--- main/uncertain/viz-seaborn.py
+++ main/uncertain/viz-seaborn.py
@@ -5,13 +5,24 @@ from argparse import ArgumentParser
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import constants
+
+def relative(x):
+    y = x['variable'].astype('datetime64[D]')
+    diff = y - y.min()
+
+    return diff.apply(lambda x: x.total_seconds() / constants.day)
 
 arguments = ArgumentParser()
 arguments.add_argument('--output', type=Path, required=True)
 args = arguments.parse_args()
 
-df = pd.read_csv(sys.stdin).melt()
-sns.lineplot(x='variable',
+df = (pd.
+      read_csv(sys.stdin)
+      .melt()
+      .assign(days=relative))
+sns.lineplot(x='days',
              y='value',
              data=df)
+plt.grid(True)
 plt.savefig(args.output)
