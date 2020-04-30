3e0157ee1ff3709e4c734a145226e7e619598cf7
Jerome White <jerome@wadhwaniai.org>
2020-04-30 12:56:55 +0000

    Separate parameter types; case sensitive config reading
    1. The parameters accepted by the model are different from the
       parameters used in the estimation. This change is an adaptor that
       supports both.
    
    2. Default Python config file reading lower cases all variable
       names. That posed a problem when used as model parameters.

--- main/uncertain/util.py
+++ main/uncertain/util.py
@@ -17,25 +17,32 @@ Logger = logging.getLogger(__name__)
 
 Split = cl.namedtuple('Split', 'train, test')
 
+class CaseConfigParser(ConfigParser):
+    def optionxform(self, optionstr):
+        return optionstr
+
+ModelParameter = cl.namedtuple('ModelParameter', [
+    'R0',
+    'T_inc',
+    'T_inf',
+    'T_recov_severe',
+    'P_severe',
+    'intervention_amount',
+])
+
 class Parameter(cl.namedtuple('Parameter', [
-        'r0',
-        't_inc',
-        't_inf',
-        't_recov_severe',
-        'p_severe',
-        'intervention_amount',
+        *ModelParameter._fields,
         'sigma',
 ])):
     __slots__ = ()
 
     @classmethod
     def from_config(cls, config, key='model'):
-        parser = ConfigParser()
+        parser = CaseConfigParser()
         parser.read(config)
-        params = parser[key]
 
         p = {}
-        for (k, v) in params.items():
+        for (k, v) in parser[key].items():
             p[k] = cls.get(*map(float, v.split(',')))
 
         return cls(**p)
@@ -49,6 +56,10 @@ class Parameter(cl.namedtuple('Parameter', [
 
     def __bool__(self):
         return all(x >= 0 for x in self)
+
+    def _asmodel(self):
+        params = map(lambda x: getattr(self, x), ModelParameter._fields)
+        return ModelParameter(*params)
 
 def dsplit(df, outlook):
     y = df.index.max() - pd.DateOffset(days=outlook)
