96ce3d981215b01b906337e2de012ec8eb15335f
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-05-04 18:06:56 +0530

    Support for getting time series from district_daily.json

--- data/processing.py
+++ data/processing.py
@@ -2,34 +2,35 @@ import pandas as pd
 import numpy as np
 import copy
 
-def get_district_time_series(dataframes, state='Karnataka', district='Bengaluru', use_district_daily=False):
-    if use_district_daily:
+def get_district_time_series(dataframes, state='Karnataka', district='Bengaluru', use_dataframe='raw_data'):
+    if use_dataframe == 'districts_daily':
         df_districts = copy.copy(dataframes['df_districts'])
-        df_districts = df_districts[np.logical_and(df_districts['state'] == state, df_districts['district'] == district)]
-        del df_districts['notes']
-        df_districts.reset_index(inplace=True, drop=True)
-        return df_districts
-    if type(dataframes) is dict:
-        df_raw_data_1 = copy.copy(dataframes['df_raw_data'])
-    else:
-        df_raw_data_1 = copy.copy(dataframes)
-    if state != None:
-        df_raw_data_1 = df_raw_data_1[df_raw_data_1['detectedstate'] == state]
-    if district != None:
-        df_raw_data_1 = df_raw_data_1[df_raw_data_1['detecteddistrict'] == district]
+        df_district = df_districts[np.logical_and(df_districts['state'] == state, df_districts['district'] == district)]
+        del df_district['notes']
+        df_district.reset_index(inplace=True, drop=True)
+        return df_district
+    if use_dataframe == 'raw_data':
+        if type(dataframes) is dict:
+            df_raw_data_1 = copy.copy(dataframes['df_raw_data'])
+        else:
+            df_raw_data_1 = copy.copy(dataframes)
+        if state != None:
+            df_raw_data_1 = df_raw_data_1[df_raw_data_1['detectedstate'] == state]
+        if district != None:
+            df_raw_data_1 = df_raw_data_1[df_raw_data_1['detecteddistrict'] == district]
 
-    df_raw_data_1['dateannounced'] = pd.to_datetime(df_raw_data_1['dateannounced'], format='%d/%m/%Y')
+        df_raw_data_1['dateannounced'] = pd.to_datetime(df_raw_data_1['dateannounced'], format='%d/%m/%Y')
 
-    index = pd.date_range(np.min(df_raw_data_1['dateannounced']), np.max(df_raw_data_1['dateannounced']))
+        index = pd.date_range(np.min(df_raw_data_1['dateannounced']), np.max(df_raw_data_1['dateannounced']))
 
-    df_district = pd.DataFrame(columns=['total_infected'], index=index)
-    df_district['total_infected'] = [0]*len(index)
-    for _, row in df_raw_data_1.iterrows():
-        try:
-            df_district.loc[row['dateannounced']:, 'total_infected'] += 1*int(row['numcases'])
-        except Exception:
-            df_district.loc[row['dateannounced']:, 'total_infected'] += 1
+        df_district = pd.DataFrame(columns=['total_infected'], index=index)
+        df_district['total_infected'] = [0]*len(index)
+        for _, row in df_raw_data_1.iterrows():
+            try:
+                df_district.loc[row['dateannounced']:, 'total_infected'] += 1*int(row['numcases'])
+            except Exception:
+                df_district.loc[row['dateannounced']:, 'total_infected'] += 1
 
-    df_district.reset_index(inplace=True)
-    df_district.columns = ['date', 'total_infected']
-    return df_district
+        df_district.reset_index(inplace=True)
+        df_district.columns = ['date', 'total_infected']
+        return df_district
