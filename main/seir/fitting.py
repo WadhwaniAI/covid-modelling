bdebad595a3341265a5eb23915a8de6e75159459
Vishwa Shah <vishwa.shah@wadhwaniai.org>
2020-06-03 18:14:29 +0530

    didn't propogate changes

--- main/seir/fitting.py
+++ main/seir/fitting.py
@@ -151,22 +151,22 @@ def smooth_big_jump(df_district, smoothing_length, data_from_tracker, method='li
     df_district = df_district.set_index('date')
     big_jump = df_district.loc[d2, 'recovered'] - df_district.loc[d1, 'recovered']
     print(big_jump)
-    if method == 'linear':
+    if smoothing_method == 'linear':
         for i, day_number in enumerate(range(smoothing_length-2, -1, -1)):
             date = datetime.datetime.strptime(d1, '%Y-%m-%d') - datetime.timedelta(days=day_number)
             offset = np.random.binomial(1, (big_jump%smoothing_length)/smoothing_length)
             df_district.loc[date, 'recovered'] += ((i+1)*big_jump)//smoothing_length + offset
             df_district.loc[date, 'hospitalised'] -= ((i+1)*big_jump)//smoothing_length + offset
 
-    elif method == 'weighted':
+    elif smoothing_method == 'weighted':
+        newcases = df_district['total_infected'].shift(14) - df_district['total_infected'].shift(15)
+        idx = newcases.first_valid_index()
+        newcases = newcases.loc[idx:]
+        truncated = df_district.loc[idx:, :]
+        invpercent = newcases.sum()/newcases
         for i, day_number in enumerate(range(smoothing_length-2, -1, -1)):
-            newcases = df_district['total_infected'].shift(14) - df_district['total_infected'].shift(15)
-            idx = newcases.first_valid_index()
-            newcases = newcases.loc[idx:]
-            truncated = df_district.loc[idx:, :]
-            invpercent = newcases.sum()/newcases
-            offset = np.random.binomial(1, (big_jump%invpercent)/newcases)
             date = datetime.datetime.strptime(d1, '%Y-%m-%d') - datetime.timedelta(days=day_number)
+            offset = np.random.binomial(1, (big_jump%invpercent.loc[date])/newcases.loc[date])
             truncated.loc[date, 'recovered'] += ((i+1)*big_jump // invpercent.loc[date]) + offset
             truncated.loc[date, 'hospitalised'] -= ((i+1)*big_jump // invpercent.loc[date]) + offset
         df_district.loc[truncated.index, 'recovered'] = truncated['recovered']
