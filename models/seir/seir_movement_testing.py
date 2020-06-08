bdf88e996128d4a8c47600d71bc36ed94e6da483
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-06-08 15:23:55 +0530

    Removed solve_ode from SEIR_Movement_Testing

--- models/seir/seir_movement_testing.py
+++ models/seir/seir_movement_testing.py
@@ -203,20 +203,6 @@ class SEIR_Movement_Testing(SEIR):
 
         return dydt
 
-    def solve_ode(self, total_days=200, time_step=1, method='Radau'):
-        """
-        Solves ODE
-        """
-        t_start = 0
-        t_final = total_days
-        time_steps = np.arange(t_start, total_days + time_step, time_step)
-        
-        state_init_values_arr = [self.state_init_values[x] for x in self.state_init_values]
-
-        sol = solve_ivp(self.get_derivative, [t_start, t_final], 
-                        state_init_values_arr, method=method, t_eval=time_steps)
-        self.sol = sol
-
     def predict(self, total_days=50, time_step=1, method='Radau'):
         """
         Returns predictions of the model
