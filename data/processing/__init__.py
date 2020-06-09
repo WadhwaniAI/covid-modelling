f7e8d6dfad7873c2b240ff7a28266e210ad0a614
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-06-09 19:36:45 +0530

    Backward compatibilty pt 3

--- data/processing/__init__.py
+++ data/processing/__init__.py
@@ -1,2 +1,2 @@
-from data.processing.processing import get_data, get_dataframes_cached
+from data.processing.processing import get_data, get_dataframes_cached, get_district_timeseries_cached
 from data.processing.ihme import bbmp, india_all, india_all_state, india_state, india_states, jhu
