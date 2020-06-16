736e257e4e791a9e5a0e25fb81ea402bf7ec68cc
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-06-16 17:28:58 +0530

    New SEIR classes added to init.py

--- models/seir/__init__.py
+++ models/seir/__init__.py
@@ -2,3 +2,6 @@ from models.seir.seir_testing import SEIR_Testing
 from models.seir.seirhd import SEIRHD
 from models.seir.seir_movement import SEIR_Movement
 from models.seir.seir_movement_testing import SEIR_Movement_Testing
+from models.seir.seirhd_severity import SEIRHD_Severity
+from models.seir.seirhd_facility import SEIRHD_Facility
+from models.seir.seirhd_bed import SEIRHD_Bed
