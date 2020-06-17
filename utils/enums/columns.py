02239ebc1efa0b8488c77aed316d1663cebd943e
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-06-18 03:53:55 +0530

    Added extra column enums for new data

--- utils/enums/columns.py
+++ utils/enums/columns.py
@@ -21,17 +21,17 @@ class Columns(Enum):
 
     date = Column('date', 'date', None)
     critical = Column('critical', 'Critical', 'brown')
-    stable_symptomatic = Column('stable_symptomatic', 'Stable Symptomatic', 'yellow')
+    stable_symptomatic = Column('stable_symptomatic', 'Stable Symptomatic', 'magenta')
     stable_asymptomatic = Column('stable_asymptomatic', 'Stable Asymptomatic', 'cyan')
     recovered = Column('recovered', 'Recovered Cases', 'green')
     deceased = Column('deceased', 'Deceased Cases', 'red')
     active = Column('hospitalised', 'Active Cases', 'orange')
     confirmed = Column('total_infected', 'Confirmed Cases', 'C0')
     
-enums = [
+compartments = [
     Column('date', 'date', None),
     Column('critical', 'Critical', 'brown'),
-    Column('stable_symptomatic', 'Stable Symptomatic', 'yellow'),
+    Column('stable_symptomatic', 'Stable Symptomatic', 'magenta'),
     Column('stable_asymptomatic', 'Stable Asymptomatic', 'cyan'),
     Column('recovered', 'Recovered Cases', 'green'),
     Column('deceased', 'Deceased Cases', 'red'),
