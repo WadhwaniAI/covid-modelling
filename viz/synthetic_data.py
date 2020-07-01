90820eb02eb7cefb19c90bc6f34dcc5c8489c6a4
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-07-01 13:15:49 +0530

    Plot with uncertainty

--- viz/synthetic_data.py
+++ viz/synthetic_data.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.lines import Line2D
+import seaborn as sns
 
 from datetime import timedelta
 
 from utils.enums import Columns
+from utils.enums.columns import *
+from viz.fit import axis_formatter
 
 
 def plot_compartment_for_datasets(ax, df_true, df_prediction, df_train, district,
@@ -125,3 +129,95 @@ def plot_all_experiments(df_true, predictions_dict, district,
 
             filename = output_folder + which_compartments[col].name
             fig.savefig(filename)
+
+
+def plot_fit_uncertainty(df_prediction, df_train, df_val, df_train_nora, df_val_nora, train_period, test_period,
+                         state, district, draws=None, which_compartments=['hospitalised', 'total_infected'],
+                         description='', savepath=None):
+    # TODO: Use plot_fit from fit.py with modifications instead
+    """Helper function for creating plots for the training pipeline
+
+    Arguments:
+        df_prediction {pd.DataFrame} -- The prediction dataframe outputted by the model
+        df_train {pd.DataFrame} -- The train dataset (with rolling average)
+        df_val {pd.DataFrame} -- The val dataset (with rolling average)
+        df_train_nora {pd.DataFrame} -- The train dataset (with no rolling average)
+        df_val_nora {pd.DataFrame} -- The val dataset (with no rolling average)
+        train_period {int} -- Length of train period
+        state {str} -- Name of state
+        district {str} -- Name of district
+
+    Keyword Arguments:
+        which_compartments {list} -- Which buckets to plot (default: {['hospitalised', 'total_infected']})
+        description {str} -- Additional description for the plots (if any) (default: {''})
+
+    Returns:
+        ax -- Matplotlib ax object
+    """
+    # Create plots
+    if isinstance(df_val, pd.DataFrame):
+        df_true_plotting_rolling = pd.concat(
+            [df_train, df_val], ignore_index=True)
+        df_true_plotting = pd.concat(
+            [df_train_nora, df_val_nora], ignore_index=True)
+    else:
+        df_true_plotting_rolling = df_train
+        df_true_plotting = df_train_nora
+    df_predicted_plotting = df_prediction.loc[df_prediction['date'].isin(
+        df_true_plotting['date']), ['date'] + which_compartments]
+
+    plot_ledger = {
+        'base': True,
+        'severity': True,
+        'bed': True
+    }
+    for i, key in enumerate(plot_ledger.keys()):
+        names = [x.name for x in compartments[key]]
+        if np.sum(np.in1d(names, which_compartments)) == 0:
+            plot_ledger[key] = False
+
+    n_rows = np.sum(list(plot_ledger.values()))
+    fig, axs = plt.subplots(nrows=n_rows, figsize=(12, 10 * n_rows))
+    fig.suptitle('{} {}, {}'.format(description, district, state))
+    i = 0
+    for key in plot_ledger.keys():
+        if not plot_ledger[key]:
+            continue
+        if n_rows > 1:
+            ax = axs[i]
+        else:
+            ax = axs
+        names = [x.name for x in compartments[key]]
+        comp_subset = np.array(which_compartments)[np.in1d(which_compartments, names)]
+        legend_elements = []
+        for compartment in compartments[key]:
+            if compartment.name in comp_subset:
+                ax.plot(df_true_plotting[compartments['date'][0].name], df_true_plotting[compartment.name],
+                        '-o', color=compartment.color, label='{} (Observed)'.format(compartment.label))
+                ax.plot(df_true_plotting_rolling[compartments['date'][0].name],
+                        df_true_plotting_rolling[compartment.name],
+                        '-', color=compartment.color, label='{} (Obs RA)'.format(compartment.label))
+                ax.plot(df_predicted_plotting[compartments['date'][0].name], df_predicted_plotting[compartment.name],
+                        '-.', color=compartment.color, label='{} (Predicted)'.format(compartment.label))
+                if draws is not None:
+                    ax.errorbar(df_prediction['date'][train_period:train_period+test_period],
+                                df_prediction[compartment.name][train_period:train_period+test_period],
+                                yerr=draws[compartment.name]['draws'][:, train_period:train_period+test_period],
+                                lw=1.0, color='lightcoral', barsabove='False', label='draws')
+
+                legend_elements.append(
+                    Line2D([0], [0], color=compartment.color, label=compartment.label))
+
+        ax.axvline(x=df_train.iloc[-train_period, :]['date'], ls=':', color='brown', label='Train starts')
+        if isinstance(df_val, pd.DataFrame) and len(df_val) > 0:
+            ax.axvline(x=df_val.iloc[0, ]['date'], ls=':', color='black', label='Val starts')
+
+        axis_formatter(ax, legend_elements, custom_legend=False)
+        i += 1
+
+        ax.legend(loc="upper left")
+
+    plt.tight_layout()
+    if savepath is not None:
+        plt.savefig(savepath)
+    return fig
