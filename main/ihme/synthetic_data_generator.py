bc427de574f786255ad128908323fe7de207e289
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-07-05 16:37:33 +0530

    Update output names, add baseline model loss

--- main/ihme/synthetic_data_generator.py
+++ main/ihme/synthetic_data_generator.py
@@ -46,17 +46,17 @@ def ihme_data_generator(district, disable_tracker, actual_start_date, dataset_le
         makesum.reset_index(), ihme_df_train, ihme_df_val, ihme_df_train_nora, ihme_df_val_nora,
         train_val_size, state, district, which_compartments=[c.name for c in Columns.curve_fit_compartments()],
         description='Train and test',
-        savepath=os.path.join(output_folder, 'ihme-i1-fit.png'))
+        savepath=os.path.join(output_folder, 'ihme_i1_fit.png'))
 
     plot_forecast_agnostic(ihme_df_true, makesum.reset_index(), model_name='IHME',
-                           dist=district, state=state, filename=os.path.join(output_folder, 'ihme-i1-forecast.png'))
+                           dist=district, state=state, filename=os.path.join(output_folder, 'ihme_i1_forecast.png'))
 
     for plot_col in Columns.curve_fit_compartments():
         plot_fit_uncertainty(makesum.reset_index(), ihme_df_train, ihme_df_val, ihme_df_train_nora, ihme_df_val_nora,
                              train_val_size, test_size, state, district, draws=ihme_res['draws'],
                              which_compartments=[plot_col.name],
                              description='Train and test',
-                             savepath=os.path.join(output_folder, f'ihme_{plot_col.name}.png'))
+                             savepath=os.path.join(output_folder, f'ihme_i1_{plot_col.name}.png'))
 
     return ihme_res, config, model_params
 
@@ -77,7 +77,7 @@ def seir_runner(district, state, input_df, data_from_tracker,
 
 def log_experiment_local(output_folder, i1_config, i1_model_params, i1_output,
                          c1_output, datasets, predictions_dicts, which_compartments,
-                         dataset_properties, series_properties):
+                         dataset_properties, series_properties, baseline_predictions_dict=None):
     params_dict = {
         'compartments_replaced': which_compartments,
         'dataset_properties': {
@@ -87,37 +87,41 @@ def log_experiment_local(output_folder, i1_config, i1_model_params, i1_output,
     }
 
     for i in datasets:
-        filename = 'dataset_' + str(i+1) + '.csv'
+        filename = 'dataset_experiment_' + str(i+1) + '.csv'
         datasets[i].to_csv(output_folder+filename)
 
     c1 = c1_output['m1']['df_loss'].T[which_compartments]
-    c1.to_csv(output_folder + "seir_loss.csv")
+    c1.to_csv(output_folder + "seir_c1_loss.csv")
 
     c1_output['m1']['ax'].savefig(output_folder + 'seir_c1.png')
 
     i1 = c1_output['m1']['df_loss'].T[which_compartments]
-    i1.to_csv(output_folder + "ihme_loss.csv")
+    i1.to_csv(output_folder + "ihme_i1_loss.csv")
 
     loss_dfs = []
     for i in predictions_dicts:
         loss_df = predictions_dicts[i]['m1']['df_loss'].T[which_compartments]
         loss_df['exp'] = i+1
         loss_dfs.append(loss_df)
-        predictions_dicts[i]['m1']['ax'].savefig(output_folder + 'seir_exp'+str(i+1)+'.png')
+        predictions_dicts[i]['m1']['ax'].savefig(output_folder + 'seir_c2_experiment_'+str(i+1)+'.png')
 
     loss = pd.concat(loss_dfs, axis=0)
     loss.index.name = 'index'
     loss.sort_values(by='index', inplace=True)
-    loss.to_csv(output_folder + "/exp_" + "_".join(which_compartments) + ".csv")
+    loss.to_csv(output_folder + "/experiments_loss.csv")
 
-    with open(output_folder + 'params.json', 'w') as outfile:
+    if baseline_predictions_dict is not None:
+        baseline_loss = baseline_predictions_dict['m1']['df_loss_s3'].T[which_compartments]
+        baseline_loss.to_csv(output_folder + "baseline_loss.csv")
+
+    with open(output_folder + 'experiments_params.json', 'w') as outfile:
         json.dump(params_dict, outfile, indent=4)
 
     i1_config['start_date'] = i1_config['start_date'].strftime("%Y-%m-%d")
-    with open(output_folder + 'ihme_config.json', 'w') as outfile:
+    with open(output_folder + 'ihme_i1_config.json', 'w') as outfile:
         json.dump(i1_config, outfile, indent=4)
 
-    with open(output_folder + 'ihme_model_params.json', 'w') as outfile:
+    with open(output_folder + 'ihme_i1_model_params.json', 'w') as outfile:
         json.dump(repr(i1_model_params), outfile, indent=4)
 
     # FIX
