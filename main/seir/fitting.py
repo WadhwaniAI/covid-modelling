fb84d3fde5ea442d73e4860ceb1b1c61d07e5779
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-07-09 02:07:46 +0530

    Changed variable param range

--- main/seir/fitting.py
+++ main/seir/fitting.py
@@ -39,7 +39,7 @@ def get_variable_param_ranges(variable_param_ranges=None, initialisation='interm
     """
     if variable_param_ranges == None:
         variable_param_ranges = {
-            'lockdown_R0': (1, 1.2),
+            'lockdown_R0': (1, 1.5),
             'T_inc': (4, 5),
             'T_inf': (3, 4),
             'T_recov_severe': (5, 60),
