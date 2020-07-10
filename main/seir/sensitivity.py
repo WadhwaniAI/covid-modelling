e511d2a20cd40c94179791ecd3fe51ed5bdd451c
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-07-10 12:32:46 +0530

    cleanup + fn name change + extra function for single comp plotting

--- main/seir/sensitivity.py
+++ main/seir/sensitivity.py
@@ -5,29 +5,32 @@ import copy
 from main.seir.optimiser import Optimiser
 
 
-def plot_sensitivity(predictions_dict, which_fit='m1', var_name=None, param_range=np.linspace(1, 100, 201),
-                     comp_name='recovered', aux_comp='total_infected'):
+def gridsearch_single_param(predictions_dict, which_fit='m1', var_name=None, param_range=np.linspace(1, 100, 201),
+                            df_train=None, train_period=None, comp_name='recovered', aux_comp='total_infected',
+                            debug=False):
     if var_name == None:
         var_name = 'T_recov_{}'.format(comp_name)
     variable_param_ranges = {
         var_name: param_range
     }
 
+    if not isinstance(df_train, pd.DataFrame):
+        df_train = copy.copy(predictions_dict[which_fit]['df_train'])
+
     default_params = copy.copy(predictions_dict[which_fit]['best_params'])
-    df_train = copy.copy(predictions_dict[which_fit]['df_train'])
-    df_val = copy.copy(predictions_dict[which_fit]['df_val'])
     run_params = copy.copy(predictions_dict[which_fit]['run_params'])
 
-    train_period = run_params['train_period']
-    val_period = run_params['val_period']
     model = run_params['model_class']
+    if train_period == None:
+        train_period = run_params['train_period']
     loss_indices = [-train_period, None]
 
-    if aux_comp == None:
+    if aux_comp == 'all':
+        which_compartments = ['recovered', 'deceased', 'hospitalised', 'total_infected']
+    elif aux_comp == None:
         which_compartments = [comp_name]
     else:
         which_compartments = [comp_name, aux_comp]
-    which_compartments = ['recovered', 'deceased', 'hospitalised', 'total_infected']
     try:
         for key in variable_param_ranges.keys():
             del default_params[key]
@@ -41,11 +44,11 @@ def plot_sensitivity(predictions_dict, which_fit='m1', var_name=None, param_rang
     loss_array, params_dict = optimiser.gridsearch(df_train, default_params, variable_param_ranges, model=model, 
                                                    method='mape', loss_indices=loss_indices, 
                                                    which_compartments=which_compartments,
-                                                   total_days=total_days, debug=False)
+                                                   total_days=total_days, debug=debug)
 
     return params_dict, loss_array
 
-def sensitivity_all_comps(predictions_dict, which_fit='m1'):
+def calculate_sensitivity_and_plot(predictions_dict, which_fit='m1'):
     var_tuples = [
         ('lockdown_R0', np.linspace(1, 1.5, 101), 'total_infected', None),
         ('I_hosp_ratio', np.linspace(0, 1, 201), 'total_infected', None),
@@ -62,8 +65,9 @@ def sensitivity_all_comps(predictions_dict, which_fit='m1'):
     fig.tight_layout(pad=5.0)
     for i, ax in enumerate(axs.flat):
         var_name, param_range, comp_name, aux_comp = var_tuples[i]
-        params_dict, loss_array = plot_sensitivity(predictions_dict, which_fit, var_name=var_name,
-                                                   param_range=param_range, comp_name=comp_name, aux_comp=aux_comp)
+        params_dict, loss_array = gridsearch_single_param(predictions_dict, which_fit, var_name=var_name,
+                                                          param_range=param_range, comp_name=comp_name, 
+                                                          aux_comp=aux_comp)
         ax.plot([x[list(x.keys())[0]] for x in params_dict], loss_array)
         ax.set_ylabel('Loss Value (MAPE)')
         ax.set_xlabel('{}'.format(var_name))
@@ -77,3 +81,14 @@ def sensitivity_all_comps(predictions_dict, which_fit='m1'):
                                     end_date=predictions_dict[which_fit]['df_val'].iloc[-1, :]['date'], 
                                     model=predictions_dict[which_fit]['run_params']['model_class'])
     return fig, best_params, df_prediction
+
+
+def plot_single_comp(params_dict, loss_array, comp_name='total_infected'):
+    fig, ax = plt.subplots(figsize=(12, 12))
+    var_name = list(params_dict[0].keys())[0]
+    ax.plot([x[list(x.keys())[0]] for x in params_dict], loss_array)
+    ax.set_ylabel('Loss Value (MAPE)')
+    ax.set_xlabel('{}'.format(var_name))
+    ax.set_title('MAPE for {} vs {} '.format(comp_name, var_name))
+    ax.grid()
+    return fig
