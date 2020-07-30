08403de7bc05ccbd5b2f1a47ef599e9deba72e71
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-07-31 02:37:55 +0530

    Add loader for new york times data

--- data/dataloader/__init__.py
+++ data/dataloader/__init__.py
@@ -2,3 +2,4 @@ from data.dataloader.covid19india import Covid19IndiaLoader
 from data.dataloader.jhu import JHULoader
 from data.dataloader.rootnet import RootnetLoader
 from data.dataloader.athena import AthenaLoader
+from data.dataloader.ny_times import NYTLoader
