014e8f20bdc59bce8e0f58ceae2d76ff099930b3
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-07-31 02:43:10 +0530

    Update get rates

--- utils/data.py
+++ utils/data.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 import sys
 sys.path.append('../..')
-from utils.population import get_district_population
+
+from utils.population import get_district_population, get_population
 from utils.enums import Columns
 
 # tuples: (district, state, census_area_name(s))
@@ -14,6 +15,8 @@ pune = 'Pune', 'Maharashtra', ['District - Pune (25)']
 delhi = 'Delhi', 'Delhi', ['State - NCT OF DELHI (07)']
 bengaluru = 'Bengaluru', 'Karnataka', ['District - Bangalore (18)', 'District - Bangalore Rural (29)']
 bengaluru_urban = 'Bengaluru Urban', 'Karnataka', ['District - Bangalore (18)']
+new_york_city = 'New York City', 'New York', None
+italy = None, 'Italy', None
 
 regions = {
     'mumbai': mumbai,
@@ -22,27 +25,37 @@ regions = {
     'pune': pune,
     'delhi': delhi,
     'bengaluru': bengaluru,
-    'bengaluru urban': bengaluru_urban
+    'bengaluru urban': bengaluru_urban,
+    'new_york_city': new_york_city,
+    'italy': italy
 }
+
 
 def get_daily_vals(df, col):
         return df[col] - df[col].shift(1)
 
-def get_rates(district_timeseries, state, area_names):
-    data = district_timeseries.set_index('date')
-    district_total_pop = get_district_population(state, area_names)
+
+def get_rates(timeseries, region, sub_region=None, area_names=None):
+    data = timeseries.set_index('date')
+    if area_names is not None:
+        total_pop = get_district_population(region, area_names)
+    else:
+        total_pop = get_population(region, sub_region=sub_region)
     for col in Columns.which_compartments():
-        data[f'{col.name}_rate'] = data[col.name]/district_total_pop
-        data[f'log_{col.name}_rate'] = data[f'{col.name}_rate'].apply(lambda x: np.log(x))
-    data.loc[:,'group'] = len(data) * [ 1.0 ]
-    data.loc[:,'covs'] = len(data) * [ 1.0 ]
+        if col.name in data.columns:
+            data[f'{col.name}_rate'] = data[col.name]/total_pop
+            data[f'log_{col.name}_rate'] = data[f'{col.name}_rate'].apply(lambda x: np.log(x))
+    data.loc[:, 'group'] = len(data) * [1.0]
+    data.loc[:, 'covs'] = len(data) * [1.0]
     data = data.reset_index()
-    data['date']= pd.to_datetime(data['date'])
-    return data, district_total_pop
+    data['date'] = pd.to_datetime(data['date'])
+    return data, total_pop
+
 
 def lograte_to_cumulative(to_transform, population):
     cumulative = np.exp(to_transform) * population
     return cumulative
+
 
 def rate_to_cumulative(to_transform, population):
     cumulative = to_transform * population
