acd1bfd65b795aa0f12129ba5607409705eae167
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-08-03 13:01:55 +0530

    Different title

--- viz/forecast.py
+++ viz/forecast.py
@@ -154,7 +154,7 @@ def plot_top_k_trials(predictions_dict, train_fit='m2', k=10, trials_processed=N
         ax.set_xlim(ax.get_xlim()[0], ax.get_xlim()[1] + 10)
         adjust_text(texts, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
         axis_formatter(ax, log_scale=log_scale)
-        fig.suptitle('Forecast - ({} {})'.format(predictions_dict['state'], predictions_dict['dist']), fontsize=16)
+        fig.suptitle('Forecast of top {} trials for {} '.format(k, compartment.name), fontsize=16)
         plots[compartment] = fig
     return plots
 
