e687ca699d8cd6430a2010f65d260861c47b5b3e
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-08-22 03:57:43 +0530

    Changed region_dict to predictions_dict

--- main/seir/uncertainty/uncertainty_base.py
+++ main/seir/uncertainty/uncertainty_base.py
@@ -4,8 +4,8 @@ import sys
 from abc import abstractmethod
 
 class Uncertainty():
-    def __init__(self, region_dict):
-        self.region_dict = region_dict
+    def __init__(self, predictions_dict):
+        self.predictions_dict = predictions_dict
     
     @abstractmethod
     def get_distribution(self):
