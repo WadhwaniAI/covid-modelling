447cf7cb289c1ec41cc0e2b4f7819928d1a1271b
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-09-05 16:29:43 +0530

    Script and runner for synthetic data experiments

---
+++ scripts/ihme_seir/runner_synth.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CFG=../seir/config
+
+cities=(
+    new_york_city
+)
+
+date=$(date '+%Y%m%d_%H%M%S')
+
+for i in ${cities[@]}; do
+    config="--region_config $CFG/$i.yaml --output_folder $date"
+    for j in `seq 0 1 120`; do
+    cat <<EOF
+python experiments_synth.py $config --num $j
+EOF
+    done
+done | parallel --will-cite --line-buffer
