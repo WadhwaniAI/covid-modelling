ed766a6e3becbe2132bc93af8d39e83ad6221cff
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-09-08 20:34:05 +0530

    Update setup

--- scripts/ihme_seir/runner_synth.sh
+++ scripts/ihme_seir/runner_synth.sh
@@ -4,13 +4,15 @@ CFG=../seir/config
 
 cities=(
     new_york_city
+    italy
+    delhi
 )
 
 date=$(date '+%Y%m%d_%H%M%S')
 
 for i in ${cities[@]}; do
     config="--region_config $CFG/$i.yaml --output_folder $date"
-    for j in `seq 0 1 120`; do
+    for j in `seq 0 1 150`; do
     cat <<EOF
 python experiments_synth.py $config --num $j
 EOF
