7d13d4c619b5fcc7363897b842d168d6f41c3b9d
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-09-18 15:10:46 +0530

    Minor update

--- scripts/seir/runner.sh
+++ scripts/seir/runner.sh
@@ -2,13 +2,13 @@
 
 CFG=config
 
-regions=(
-    new_york_city
-    italy
-    delhi
-)
-
-date=$(date '+%Y%m%d_%H%M%S')
+#regions=(
+#    new_york_city
+#    italy
+#    delhi
+#)
+#
+#date=$(date '+%Y%m%d_%H%M%S')
 
 #for i in ${regions[@]}; do
 #    config="--region_config $CFG/$i.yaml --output_folder $date"
