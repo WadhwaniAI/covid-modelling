665813408519c0639c41dd75e50fda89f3a60715
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-09-18 14:54:23 +0530

    Update runners

--- scripts/ihme/runner.sh
+++ scripts/ihme/runner.sh
@@ -3,18 +3,37 @@
 CFG=config
 
 regions=(
-    new_york_city
-    italy
-    delhi
+  delhi
+  italy
+  new_york_city
+  los_angeles
+  pune
+)
+
+starts=(
+  91
+  138
+  121
+  134
+  73
+)
+
+ends=(
+  142
+  181
+  170
+  175
+  114
 )
 
 date=$(date '+%Y%m%d_%H%M%S')
-
+count=0
 for i in ${regions[@]}; do
     config="--region_config $CFG/$i.yaml --output_folder $date"
-    for j in `seq 0 1 170`; do
+    for j in `seq ${starts[count]} 1 ${ends[count]}`; do
     cat <<EOF
 python experiments.py $config --num $j
 EOF
     done
+    count=$((count+1))
 done | parallel --will-cite --line-buffer
--- scripts/ihme_seir/runner_synth.sh
+++ scripts/ihme_seir/runner_synth.sh
@@ -2,19 +2,53 @@
 
 CFG=../seir/config
 
-cities=(
-    new_york_city
-    italy
-    delhi
+regions=(
+  delhi
+  italy
+  new_york_city
+  los_angeles
+  pune
+)
+
+starts=(
+  91
+  138
+  121
+  134
+  73
+)
+
+ends=(
+  142
+  181
+  170
+  175
+  114
 )
 
 date=$(date '+%Y%m%d_%H%M%S')
-
-for i in ${cities[@]}; do
+count=0
+for i in ${regions[@]}; do
     config="--region_config $CFG/$i.yaml --output_folder $date"
-    for j in `seq 0 1 150`; do
+    for j in `seq ${starts[count]} 1 ${ends[count]}`; do
     cat <<EOF
-python experiments_synth.py $config --num $j
+python experiments.py $config --num $j
 EOF
     done
+    count=$((count+1))
 done | parallel --will-cite --line-buffer
+
+#cities=(
+#    los_angeles
+#)
+#
+#date=$(date '+%Y%m%d_%H%M%S')
+#
+#for i in ${cities[@]}; do
+#    config="--region_config $CFG/$i.yaml --output_folder $date"
+#    for j in `seq 0 1 100`; do
+#    cat <<EOF
+#python experiments_synth.py $config --num $j
+#EOF
+#    done
+#done | parallel --will-cite --line-buffer
