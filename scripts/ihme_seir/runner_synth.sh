226367d3d3f29bde2ba98c7272adc84f45f304da
nayana-wiai <nayana.bannur@wadhwaniai.org>
2020-09-23 12:22:53 +0530

    Update runners

--- scripts/ihme_seir/runner_synth.sh
+++ scripts/ihme_seir/runner_synth.sh
@@ -37,18 +37,3 @@ EOF
     done
     count=$((count+1))
 done | parallel --will-cite --line-buffer
-
-#cities=(
-#    los_angeles
-#)
-#
-#date=$(date '+%Y%m%d_%H%M%S')
-#
-#for i in ${cities[@]}; do
-#    config="--region_config $CFG/$i.yaml --output_folder $date"
-#    for j in `seq 0 1 100`; do
-#    cat <<EOF
-#python experiments_synth.py $config --num $j
-#EOF
-#    done
-#done | parallel --will-cite --line-buffer
--- scripts/seir/runner.sh
+++ scripts/seir/runner.sh
@@ -2,45 +2,28 @@
 
 CFG=config
 
-#regions=(
-#    new_york_city
-#    italy
-#    delhi
-#)
-#
-#date=$(date '+%Y%m%d_%H%M%S')
-
-#for i in ${regions[@]}; do
-#    config="--region_config $CFG/$i.yaml --output_folder $date"
-#    for j in `seq 0 1 150`; do
-#    cat <<EOF
-#python experiments.py $config --num $j
-#EOF
-#    done
-#done | parallel --will-cite --line-buffer
-
 regions=(
   delhi
   italy
   new_york_city
-  los_angeles
-  pune
+#  los_angeles
+#  pune
 )
 
 starts=(
-  91
-  138
-  121
-  134
-  73
+  0
+  0
+  0
+#  0
+#  0
 )
 
 ends=(
   142
   181
   170
-  175
-  114
+#  175
+#  114
 )
 
 date=$(date '+%Y%m%d_%H%M%S')
