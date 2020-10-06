725490e2b69c2f0d74d75e40206024b03b6ee5a3
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-10-07 02:23:29 +0530

    New fxn : load from daily_reports + minor refactor

--- data/dataloader/jhu.py
+++ data/dataloader/jhu.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+import datetime
+
 from data.dataloader.base import BaseLoader
 
 class JHULoader(BaseLoader):
@@ -23,16 +25,35 @@ class JHULoader(BaseLoader):
         df['Date'] = pd.to_datetime(df['Date'])
         return df
 
+    def _load_from_daily_reports(self):
+        starting_date = datetime.datetime.strptime('04-12-2020', "%m-%d-%Y")
+        main_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/{}.csv'
+
+        total_days = (datetime.datetime.today() - starting_date).days
+        df_master = pd.read_csv(main_url.format(starting_date.strftime("%m-%d-%Y")))
+        df_master['date'] = starting_date
+        for i in range(1, total_days+1):
+            curr_date = starting_date + datetime.timedelta(days=i)
+            try:
+                df = pd.read_csv(main_url.format(curr_date.strftime("%m-%d-%Y")))
+                df['date'] = curr_date
+                df_master = pd.concat([df_master, df], ignore_index=True)
+            except Exception as e:
+                pass
+
+        return df_master
+
     # Loads time series case data for every country (and all provinces within certain countries) from JHU's github repo
-    def load_data(self):
+    def _load_data_from_time_series_global(self):
         """
         This function parses the confirmed, death and recovered CSVs on JHU's github repo and converts them to pandas dataframes
         Columns of returned dataframe : 
         ['Province/State', 'Country/Region', 'Lat', 'Long', 'Date', 'ConfirmedCases', 'Deaths', 'RecoveredCases', 'ActiveCases']
         """
-        df_confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-        df_death = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-        df_recovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+        main_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{}_global.csv'
+        df_confirmed = pd.read_csv(main_url.format('confirmed'))
+        df_death = pd.read_csv(main_url.format('deaths'))
+        df_recovered = pd.read_csv(main_url.format('recovered'))
 
         df_confirmed = self._modify_dataframe(df_confirmed, 'ConfirmedCases')
         df_death = self._modify_dataframe(df_death, 'Deaths')
@@ -43,5 +64,16 @@ class JHULoader(BaseLoader):
 
         return df_master
 
+    def load_data(self):
+        dataframes = {}
+        dataframes['df_us_states'] = self._load_from_daily_reports()
+        dataframes['df_global'] = self._load_data_from_time_series_global()
+
+        return dataframes
+
     def get_jhu_data(self):
         return self.load_data()
+
+if __name__ == '__main__':
+    obj = JHULoader()
+    df_states = obj._load_from_daily_reports()
