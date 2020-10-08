21b839d2f35af876db4d29ec26a511f137ca4a90
Siddhant Shingi <siddhantshingi@Siddhants-MacBook-Pro.local>
2020-10-08 14:44:45 +0530

    [WIP] Code structed formated

---
+++ data/dataloader/simulated.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from models.seir import *
+
+from data.dataloader.base import BaseLoader
+
+class SimulatedDataLoader(BaseLoader):
+    def __init__(self):
+        super().__init__()
+    
+    def generate_simulated_data(**config):
+        """generates simulated data using the input params in config
+
+        Keyword Arguments
+        -----------------
+            config {dict} -- keys required:
+                model {str} -- Name of model to use to generate the data (in title case)
+                starting_date {datetime} -- Starting date of the simulated data (in YYYY-MM-DD format)
+                total_days {int} -- Number of days for which simulated data has to be generated (default: 50)
+                initial_values {dict} -- Initial values for 'Active', 'Recovered' and 'Deceased' bucket
+                params {dict} -- Parameters to generate the simulated data
+        
+        Returns
+        -------
+            pd.DataFrame -- dataframe of cases for a particular state, district with 5 columns : 
+                ['date', 'total', 'active', 'deceased', 'recovered']
+        """
+        model_params = config['params']
+        model_params['starting_date'] = config['starting_date']
+
+        initial_colums = ['active', 'recovered', 'deceased']
+        observed_values = {col : config['initial_values'][col] for col in initial_colums}
+        observed_values['total'] = sum(observed_values.values())
+        observed_values['date'] =  config['starting_date']
+        model_params['observed_values'] = pd.DataFrame.from_dict([observed_values]).iloc[0,:]
+        
+        solver = eval(config['model'])(**model_params)
+        if (config['total_days']):
+            df_result = solver.predict(total_days=config['total_days'])
+        else:
+            df_result = solver.predict()
+        return df_result
