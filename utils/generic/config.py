57927523272f36b05d71b7654d733581c1e6dda2
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-10-13 03:06:40 +0530

    Fn for converting nested dict date keys to str

--- utils/generic/config.py
+++ utils/generic/config.py
@@ -1,15 +1,21 @@
 import yaml
 import os
+import copy
 
 import models
 import main.seir.uncertainty as uncertainty_module
 from utils.generic.enums import Columns
 
+import datetime
 
-def read_config(filename='default.yaml'):
+
+def read_config(filename='default.yaml', preprocess=True):
     with open(f'../../configs/seir/{filename}') as configfile:
         config = yaml.load(configfile, Loader=yaml.SafeLoader)
-
+    
+    if not preprocess:
+        return config
+    
     config['fitting']['model'] = getattr(models.seir, config['fitting']['model'])
 
     config['forecast']['plot_topk_trials_for_columns'] = [Columns.from_name(
@@ -22,3 +28,20 @@ def read_config(filename='default.yaml'):
         config['uncertainty']['uncertainty_params']['sort_trials_by_column'])
         
     return config
+
+def make_date_key_str(config):
+    keys_to_make_str = []
+    new_config = copy.copy(config)
+    for k, v in config.items():
+        if isinstance(v, dict):
+            new_config[k] = make_date_key_str(v)
+        else:
+            if isinstance(k, datetime.date):
+                keys_to_make_str.append(k)
+
+    for k, v in config.items():
+        if k in keys_to_make_str:
+            new_config[k.strftime('%Y-%m-%d')] = v
+            del new_config[k]
+    
+    return new_config
