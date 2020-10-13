40a4ee00d8caf995d0bd994939dbb48febb23df6
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-10-13 12:38:21 +0530

    Logging top 10 trials and deciles dfs in w&b

--- utils/generic/logging.py
+++ utils/generic/logging.py
@@ -2,6 +2,38 @@ import wandb
 import copy
 import matplotlib as mpl
 
+import numpy as np
+import pandas as pd
+
+def create_dataframes_for_logging(m2_dict):
+    trials_processed = copy.deepcopy(m2_dict['trials_processed'])
+    trials_processed['losses'] = np.around(trials_processed['losses'], 2)
+    trials_processed['params'] = [{key: np.around(value, 2) for key, value in params_dict.items()}
+                                   for params_dict in trials_processed['params']]
+    df = pd.DataFrame.from_dict({(i+1, trials_processed['losses'][i]): trials_processed['params'][i]
+                                 for i in range(10)})
+    df_trials_params = copy.deepcopy(df)
+
+    deciles = copy.deepcopy(m2_dict['deciles'])
+    for key in deciles.keys():
+        deciles[key]['params'] = {param: np.around(value, 2) 
+                                  for param, value in deciles[key]['params'].items()}
+
+    for key in deciles.keys():
+        deciles[key]['df_loss'] = deciles[key]['df_loss'].astype(
+            float).round(2)
+
+    df = pd.DataFrame.from_dict({np.around(key, 1): deciles[key]['params'] 
+                                 for key in deciles.keys()})
+    df_deciles_params = copy.deepcopy(df)
+
+    df = pd.DataFrame.from_dict({np.around(key, 1): deciles[key]['df_loss'].to_dict()['train']
+                                 for key in deciles.keys()})
+    df_decile_losses = copy.deepcopy(df)
+
+    return df_trials_params, df_deciles_params, df_decile_losses
+
+
 def log_wandb(predictions_dict):
     # Logging all input data dataframes
     dataframes_to_log = ['df_train', 'df_val', 'df_district', 'df_district_unsmoothed']
@@ -11,7 +43,8 @@ def log_wandb(predictions_dict):
             if df is not None:
                 df['date'] = df['date'].dt.strftime('%Y-%m-%d')
             wandb.log({f"{run}/{df_name}": wandb.Table(dataframe=df)})
-        wandb.log({f"{run}/{'df_loss'}": wandb.Table(dataframe=predictions_dict[run]['df_loss'])})
+        wandb.log({f"{run}/{'df_loss'}": wandb.Table(
+            dataframe=predictions_dict[run]['df_loss'].reset_index())})
 
     # Logging all forecast data dataframes
     for forecast_name in predictions_dict['m2']['forecasts'].keys():
@@ -20,6 +53,15 @@ def log_wandb(predictions_dict):
             df['date'] = df['date'].dt.strftime('%Y-%m-%d')
         wandb.log({f"{'forecast_dataframes'}/{forecast_name}": wandb.Table(dataframe=df)})
 
+    df_trials_params, df_deciles_params, df_decile_losses = create_dataframes_for_logging(
+        predictions_dict['m2'])
+
+    wandb.log({f"{'deciles'}/{'trials_params'}": wandb.Table(
+        dataframe=df_trials_params.reset_index())})
+    wandb.log({f"{'deciles'}/{'deciles_params'}": wandb.Table(
+        dataframe=df_deciles_params.reset_index())})
+    wandb.log({f"{'deciles'}/{'decile_losses'}": wandb.Table(
+        dataframe=df_decile_losses.reset_index())})
 
     # Logging all non forecast plots
     for run in ['m1', 'm2']:
