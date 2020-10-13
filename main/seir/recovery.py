fc26ebd7697303123c81e3c0c1c917f586aac594
Siddhant Shingi <siddhantshingi@Siddhants-MacBook-Pro.local>
2020-10-13 21:54:30 +0530

    updated loss function for parameter recvoery

--- main/seir/recovery.py
+++ main/seir/recovery.py
@@ -5,10 +5,14 @@ def loss_parameter_recovery(param_true, param_pred, loss_function = 'rsme'):
     df = pd.DataFrame([param_true, param_pred])
     
     loss = Loss_Calculator()
-    if (loss_function == 'rsme'):
-        return loss._calc_rmse(df.iloc[1,:], df.iloc[0,:])
-    elif (loss_function == 'rmse_log'):
-        return loss._calc_rmse(df.iloc[1,:], df.iloc[0,:], log=True)
-    elif (loss_function == 'mape'):
-        return loss._calc_mape(df.iloc[1,:], df.iloc[0,:])
-    
+    if loss_function == 'rmse':
+        calculate = lambda x, y : loss._calc_rmse(x, y)
+    if loss_function == 'rmse_log':
+        calculate = lambda x, y : loss._calc_rmse(x, y, log=True)
+    if loss_function == 'mape':
+        calculate = lambda x, y : loss._calc_mape(x, y)
+
+    loss_dict = {}
+    for i, param in enumerate(df.columns):
+        loss_dict[param] = calculate(df.iloc[1,i], df.iloc[0,i])
+    return loss_dict    
