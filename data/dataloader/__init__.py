4a3de06f37a5238d5e0ca0792ed4b708f2b1ade6
Siddhant Shingi <siddhantshingi@Siddhants-MacBook-Pro.local>
2020-10-21 13:20:50 +0530

    simulated_data function added

--- data/dataloader/__init__.py
+++ data/dataloader/__init__.py
@@ -4,3 +4,4 @@ from data.dataloader.ny_times import NYTLoader
 from data.dataloader.covidtracking import CovidTrackingLoader
 from data.dataloader.rootnet import RootnetLoader
 from data.dataloader.athena import AthenaLoader
+from data.dataloader.simulated import SimulatedDataLoader
