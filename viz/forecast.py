a51d894481a2a7b6e3057b4cb3bc68da3fb54b9a
ee18b153 <ee18b153@smail.iitm.ac.in>
2020-10-27 16:18:13 +0530

    uncommenting forecast

--- viz/forecast.py
+++ viz/forecast.py
@@ -68,9 +68,6 @@ def plot_forecast(predictions_dict: dict, region: tuple, fits_to_plot=['best'], 
     predictions = []
     for i, forecast in enumerate(fits_to_plot):
         predictions.append(predictions_dict['m2']['forecasts'][fits_to_plot[i]])
-        
-    # For exploration    
-    print(predictions)
     
     # Extracting the true data for the district
     df_true = predictions_dict['m1']['df_district']
@@ -81,9 +78,6 @@ def plot_forecast(predictions_dict: dict, region: tuple, fits_to_plot=['best'], 
                                                        which_compartments)
 
     fig, ax = plt.subplots(figsize=(12, 12))
-
-    # For exploration
-    # print(compartments)
     
     for compartment in compartments['base']:
         if compartment.name in which_compartments:
