64e043f0802a912b00742b52ef924b1b0747606a
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-11-02 15:34:47 +0530

    New functions for creating scatter plot

--- viz/reichlab.py
+++ viz/reichlab.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+import geoplot as gplt
+import geoplot.crs as gcrs
+
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 import seaborn as sns
 
@@ -18,7 +22,7 @@ def calculate_z_score(df_mape, df_rank, model_name='Wadhwani_AI'):
 def create_heatmap(df, var_name='z_score', center=0):
     fig, ax = plt.subplots(figsize=(12, 12))
 
-    df_sorted = df.sort_values(var_name)
+    df_sorted = df.sort_values('z_score')
     annot_mat = df_sorted.index.to_numpy().reshape(-1, 3) + " : " + \
     np.around(df_sorted[var_name].to_numpy().reshape(-1, 3), 2).astype(str)
     annot_mat = annot_mat.astype(str).tolist()
@@ -26,6 +30,7 @@ def create_heatmap(df, var_name='z_score', center=0):
                 cmap='coolwarm', center=center, xticklabels=False,
                 yticklabels=False, annot=annot_mat, fmt='', ax=ax)
 
+    ax.set_title(f'Heatmap of {var_name} for all US states, sorted by z_score, cmap centered at {center}')
     return fig, ax
 
 
@@ -39,4 +44,40 @@ def create_geoplot_choropleth(df):
         cmap='coolwarm', legend=True,
         ax=ax
     )
+    return fig
+
+def combine_with_train_error(predictions_dict, df):
+    df_wadhwani = pd.DataFrame(index=list(predictions_dict.keys()),
+                               columns=['best_loss_train', 'test_loss', 
+                                        'T_recov_fatal', 'P_fatal'])
+    for loc in predictions_dict.keys():
+        df_wadhwani.loc[loc, 'best_loss_train'] = predictions_dict[loc]['m2']['df_loss'].to_numpy()[0][0]
+        df_wadhwani.loc[loc, 'T_recov_fatal'] = predictions_dict[loc]['m2']['best_params']['T_recov_fatal']
+        df_wadhwani.loc[loc, 'P_fatal'] = predictions_dict[loc]['m2']['best_params']['P_fatal']
+
+    df_wadhwani = df_wadhwani.merge(df, left_index=True, right_index=True)
+
+    df_wadhwani.drop(['Northern Mariana Islands', 'Guam',
+                      'Virgin Islands'], axis=0, inplace=True, errors='ignore')
+    
+    return df_wadhwani
+
+def create_scatter_plot_mape(df_wadhwani, annotate=True):
+    fig, ax = plt.subplots(figsize=(12, 12))
+    df_zpos = df_wadhwani[df_wadhwani['z_score'] > 0]
+    df_zneg = df_wadhwani[df_wadhwani['z_score'] < 0]
+    ax.scatter(df_zpos['best_loss_train'], df_zpos['model_mape'],
+            s=df_zpos['z_score']*50, c='red', marker='o', label='+ve Z score states')
+    ax.scatter(df_zneg['best_loss_train'], df_zneg['model_mape'], s=-
+            df_zneg['z_score']*50, c='blue', marker='o', label='-ve Z score states')
+    if annotate:
+        for i, (index, row) in enumerate(df_wadhwani.iterrows()):
+            ax.annotate(f'{index} ({round(row["z_score"], 2)})', 
+                        (row['best_loss_train'], row['model_mape']))
+    ax.set_xlabel('MAPE on training data (calculated daily)')
+    ax.set_ylabel('MAPE on unseen data (calculated weekly)')
+    ax.axvline(1, ls=':', c='red', label='train error threshold (1%)')
+    ax.axhline(5, ls=':', c='blue', label='test error threshold (5%)')
+    ax.legend()
+    ax.set_title(f'Scatter plot of train vs test error, point radii proportional to z_score')
     return fig
