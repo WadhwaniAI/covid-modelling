ed8b6d6666445d519bc52bf1ce8c9c2eab961b9f
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-11-26 17:54:21 +0530

    fn for plotting ptiles of reichlab submissions

--- viz/uncertainty.py
+++ viz/uncertainty.py
@@ -5,7 +5,7 @@ import numpy as np
 import seaborn as sns
 from adjustText import adjust_text
 import datetime
-import copy
+from copy import copy
 
 from utils.generic.enums import Columns, SEIRParams
 from viz.utils import axis_formatter
@@ -13,7 +13,7 @@ from viz.utils import axis_formatter
 
 def plot_ptiles(predictions_dict, train_fit='m2', vline=None, which_compartments=[Columns.active], 
                 plot_individual_curves=True, log_scale=False):
-    predictions = copy.copy(predictions_dict[train_fit]['forecasts'])
+    predictions = copy(predictions_dict[train_fit]['forecasts'])
     del predictions['best']
 
     df_master = list(predictions.values())[0]
@@ -49,6 +49,38 @@ def plot_ptiles(predictions_dict, train_fit='m2', vline=None, which_compartments
         plots[compartment] = fig
     
     return plots
+
+
+def plot_ptiles_reichlab(df_comb, model, location, compartment='deceased', plot_individual_curves=True):
+
+    compartment = Columns.from_name(compartment)
+    df_plot = copy(df_comb.loc[(df_comb['model'] == model) & (df_comb['location_name'] == location), :])
+    fig, ax = plt.subplots(figsize=(12, 12))
+    texts = []
+    df_true = df_plot.groupby('target_end_date').mean().reset_index()
+    ax.plot(df_true['target_end_date'].to_numpy(), df_true['true_value'].to_numpy(),
+            '--o', color=compartment.color, label=f'{compartment.label} (Observed)')
+    df_quantiles = df_plot[df_plot['type'] == 'quantile']
+    quantiles = df_quantiles.groupby('quantile').sum().index
+    if plot_individual_curves:
+        for _, qtile in enumerate(quantiles):
+            df_qtile = df_quantiles[df_quantiles['quantile'] == qtile]
+            sns.lineplot(x=Columns.date.name, y='forecast_value', data=df_qtile,
+                         ls='-', label=f'{compartment.label} Percentile :{round(qtile*100)}')
+            texts.append(plt.text(
+                x=df_qtile[Columns.date.name].iloc[-1], 
+                y=df_qtile['forecast_value'].iloc[-1], s=round(qtile*100)))
+    else:
+        sns.lineplot(x=Columns.date.name, y='forecast_value', data=df_quantiles,
+                        ls='-', label=f'{compartment.label}')
+            
+
+    ax.set_xlim(ax.get_xlim()[0], ax.get_xlim()[1] + 10)
+    adjust_text(texts, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
+    axis_formatter(ax)
+    fig.suptitle('Forecast of all deciles for {}, {}, {}'.format(model, location, compartment.label), fontsize=16)
+    
+    return fig, ax
 
 def plot_beta_loss(dict_of_trials):
     fig, ax = plt.subplots(figsize=(12, 8))
