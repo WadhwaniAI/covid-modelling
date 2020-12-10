d07da7c6d5a0d2728a558694a8baec6f62db62cb
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-12-10 21:21:30 +0530

    Custom legend + enchanced rounding

--- viz/uncertainty.py
+++ viz/uncertainty.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.lines import Line2D
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -7,7 +8,7 @@ from adjustText import adjust_text
 import datetime
 from copy import copy
 
-from utils.generic.enums import Columns, SEIRParams
+from utils.generic.enums import Columns
 from viz.utils import axis_formatter
 
 
@@ -55,30 +56,41 @@ def plot_ptiles_reichlab(df_comb, model, location, compartment='deceased', plot_
 
     compartment = Columns.from_name(compartment)
     df_plot = copy(df_comb.loc[(df_comb['model'] == model) & (df_comb['location_name'] == location), :])
+    df_plot.rename({'forecast_value': 'forecast value'}, axis=1, inplace=True)
     fig, ax = plt.subplots(figsize=(12, 12))
     texts = []
     df_true = df_plot.groupby('target_end_date').mean().reset_index()
     ax.plot(df_true['target_end_date'].to_numpy(), df_true['true_value'].to_numpy(),
-            '--o', color=compartment.color, label=f'{compartment.label} (Observed)')
+            '--o', color=compartment.color)
     df_quantiles = df_plot[df_plot['type'] == 'quantile']
     quantiles = df_quantiles.groupby('quantile').sum().index
     if plot_individual_curves:
         for _, qtile in enumerate(quantiles):
             df_qtile = df_quantiles[df_quantiles['quantile'] == qtile]
-            sns.lineplot(x=Columns.date.name, y='forecast_value', data=df_qtile,
-                         ls='-', label=f'{compartment.label} Percentile :{round(qtile*100)}')
+            label = round(qtile*100) if qtile*100 % 1 < 1e-8 else round(qtile*100, 1)
+            sns.lineplot(x=Columns.date.name, y='forecast value', data=df_qtile,
+                         ls='-')
             texts.append(plt.text(
                 x=df_qtile[Columns.date.name].iloc[-1], 
-                y=df_qtile['forecast_value'].iloc[-1], s=round(qtile*100)))
+                y=df_qtile['forecast value'].iloc[-1], s=label))
     else:
-        sns.lineplot(x=Columns.date.name, y='forecast_value', data=df_quantiles,
+        sns.lineplot(x=Columns.date.name, y='forecast value', data=df_quantiles,
                         ls='-', label=f'{compartment.label}')
             
 
     ax.set_xlim(ax.get_xlim()[0], ax.get_xlim()[1] + 10)
     adjust_text(texts, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
     axis_formatter(ax)
-    fig.suptitle('Forecast of all deciles for {}, {}, {}'.format(model, location, compartment.label), fontsize=16)
+    legend_elements = [
+        Line2D([0], [0], ls='--', marker='o', color=compartment.color,
+               label=f'Cumulative {compartment.label} (Observed)'),
+        Line2D([0], [0], ls='-', color='blue', 
+               label=f'Cumulative {compartment.label} Percentiles'),
+    ]
+    ax.legend(handles=legend_elements)
+    fig.suptitle('Forecast of all deciles for {}, {}, {}'.format(model.replace('_', '\_'), 
+                                                                 location, 
+                                                                 "Cumulative " + compartment.label), fontsize=16)
     fig.subplots_adjust(top=0.96)
     
     return fig, ax
