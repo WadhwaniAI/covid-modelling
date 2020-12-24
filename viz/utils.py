ea6fdc5829d560550e4c3f71dc7621055d9adfb4
Sansiddh Jain <sansiddh@wadhwaniai.org>
2020-12-24 19:58:54 +0530

    Minor fix in axis_formatter

--- viz/utils.py
+++ viz/utils.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 from matplotlib.lines import Line2D
 
 
-def axis_formatter(ax, legend_elements=None, custom_legend=False, log_scale=False):
+def axis_formatter(ax, legend_elements=None, custom_legend=False, append_leg_elements=False, log_scale=False):
     """Helper function for formatting axis
 
     Arguments:
@@ -15,20 +15,21 @@ def axis_formatter(ax, legend_elements=None, custom_legend=False, log_scale=Fals
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
     ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    ax.set_ylabel('No of People')
+    ax.set_ylabel('No of Cases')
     ax.set_xlabel('Time')
     ax.tick_params('x', labelrotation=45)
     ax.grid()
     if log_scale:
         ax.set_yscale('log')
     if custom_legend:
-        legend_elements += [
-            Line2D([0], [0], ls='-', marker='o', color='black', label='Observed'),
-            Line2D([0], [0], ls = '-', color='black', label='Observed Roll Avg'), 
-            Line2D([0], [0], ls='-.', color='black', label='Predicted'),
-            Line2D([0], [0], ls=':', color='brown', label='Train starts'),
-            Line2D([0], [0], ls=':', color='black', label='Val starts')
-            ]
+        if append_leg_elements:
+            legend_elements += [
+                Line2D([0], [0], ls='-', marker='o', color='black', label='Observed'),
+                Line2D([0], [0], ls = '-', color='black', label='Observed Roll Avg'), 
+                Line2D([0], [0], ls='-.', color='black', label='Predicted'),
+                Line2D([0], [0], ls=':', color='brown', label='Train starts'),
+                Line2D([0], [0], ls=':', color='black', label='Val starts')
+                ]
         ax.legend(handles=legend_elements)
     else:
         ax.legend()
