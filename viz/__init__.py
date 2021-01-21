c75eb2d162c70a847d4fe7df27afc241c8fd3795
Siddhant Shingi <siddhant@wadhwaniai.org>
2021-01-21 14:39:34 +0530

    remove viz.testing from __init__.py

--- viz/__init__.py
+++ viz/__init__.py
@@ -2,6 +2,5 @@ from viz.backtesting import *
 from viz.data import *
 from viz.fit import *
 from viz.forecast import *
-from viz.testing import *
 from viz.ihme import *
 from viz.uncertainty import *
