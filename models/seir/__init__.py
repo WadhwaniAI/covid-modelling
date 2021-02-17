fcc4b634b4cc5ecd1e17bef8ed0010979e770aa4
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-02-17 17:12:33 +0530

    Deleted legacy SEIR model

--- models/seir/__init__.py
+++ models/seir/__init__.py
@@ -2,7 +2,6 @@ from models.seir.seir_pu import SEIR_PU
 from models.seir.seir_undetected import SEIR_Undetected
 from models.seir.seir_testing import SEIR_Testing
 from models.seir.seirhd import SEIRHD
-from models.seir.seirhd_t import SEIRHD_t
 from models.seir.seir_movement import SEIR_Movement
 from models.seir.seir_movement_testing import SEIR_Movement_Testing
 from models.seir.seirhd_severity import SEIRHD_Severity
