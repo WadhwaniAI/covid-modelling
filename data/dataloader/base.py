130adef37e5f9ab78e0ce5d163bc45bacc5d6833
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-02-17 17:11:09 +0530

    Moved get_dfs_cached to base + rename

--- data/dataloader/base.py
+++ data/dataloader/base.py
@@ -1,9 +1,36 @@
 from abc import ABC, abstractmethod
 
+import os
+import datetime
+import pickle
+
 class BaseLoader(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
-    def load_data(self):
+    def pull_dataframes(self):
         pass
+
+    @abstractmethod
+    def pull_dataframes_cached(self, reload_data=False, label=None, **kwargs):
+        os.makedirs("../../misc/cache/", exist_ok=True)
+
+        loader_key = self.__class__.__name__
+        label = '' if label is None else f'_{label}'
+        picklefn = "../../misc/cache/dataframes_ts_{today}_{loader_key}{label}.pkl".format(
+            today=datetime.datetime.today().strftime("%d%m%Y"), loader_key=loader_key, label=label)
+        if reload_data:
+            print("pulling from source")
+            dataframes = self.pull_dataframes(**kwargs)
+        else:
+            try:
+                with open(picklefn, 'rb') as pickle_file:
+                    dataframes = pickle.load(pickle_file)
+                print(f'loading from {picklefn}')
+            except:
+                print("pulling from source")
+                dataframes = self.pull_dataframes(**kwargs)
+                with open(picklefn, 'wb+') as pickle_file:
+                    pickle.dump(dataframes, pickle_file)
+        return dataframes
