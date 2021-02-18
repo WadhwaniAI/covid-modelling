c255a34b257643bd765f415aae20d8660a54e4f6
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-02-18 11:46:15 +0530

    Added stratifed pulling funcs to athena

--- data/dataloader/athena.py
+++ data/dataloader/athena.py
@@ -11,7 +11,7 @@ class AthenaLoader(BaseLoader):
     def __init__(self):
         super().__init__()
 
-    def create_connection(self, schema, staging_dir, pyathena_rc_path=None):
+    def create_connection(self, schema, staging_dir, pyathena_rc_path='../../misc/pyathena/pyathena.rc'):
         """Creates SQL Server connection using AWS Athena credentials
 
         Keyword Arguments:
@@ -20,8 +20,6 @@ class AthenaLoader(BaseLoader):
         Returns:
             [cursor] -- [Connection Cursor]
         """
-        if pyathena_rc_path is None:
-            pyathena_rc_path = '../../misc/pyathena/pyathena.rc'
         SCHEMA_NAME = schema
 
         # Open Pyathena RC file and get list of all connection variables in a processable format
@@ -49,7 +47,8 @@ class AthenaLoader(BaseLoader):
         return cursor
 
 
-    def pull_dataframes(self, schema, tables, staging_dir, pyathena_rc_path=None, **kwargs):
+    def pull_dataframes(self, schema, tables, staging_dir, 
+                        pyathena_rc_path='../../misc/pyathena/pyathena.rc', **kwargs):
         """Creates connection to Athena database and returns all the tables there as a dict of Pandas dataframes
 
         Keyword Arguments:
@@ -65,8 +64,6 @@ class AthenaLoader(BaseLoader):
             healthcare_capacity
             testing_summary
         """
-        if pyathena_rc_path is None:
-            pyathena_rc_path = '../../misc/pyathena/pyathena.rc'
 
         # Create connection
         cursor = self.create_connection(schema, staging_dir, pyathena_rc_path)
@@ -81,11 +78,17 @@ class AthenaLoader(BaseLoader):
 
     def pull_dataframes_cached(self, reload_data=False, label=None, **kwargs):
         return super().pull_dataframes_cached(reload_data=reload_data, label=label, **kwargs)
+    
+    def get_data(self, **dataloading_params):
+        if (dataloading_params['stratified_data']):
+            return self.get_data_stratified(**dataloading_params)
+        else:
+            return self.get_data_standard(**dataloading_params)
 
-    def get_data(self, state='Maharashtra', district='Mumbai', reload_data=False, **kwargs):
+    def get_data_standard(self, state='Maharashtra', district='Mumbai', reload_data=False, **kwargs):
         print('fetching from athenadb...')
         label = kwargs.pop('label', None)
-        dataframes = self.pull_dataframes_cached(reload_data=reload_data, **kwargs)
+        dataframes = self.pull_dataframes_cached(reload_data=reload_data, label=label, ** kwargs)
         df_result = copy.copy(dataframes['case_summaries'])
         df_result.rename(columns={'deaths': 'deceased', 'total cases': 'total',
                                 'active cases': 'active', 'recoveries': 'recovered'}, inplace=True)
@@ -100,3 +103,49 @@ class AthenaLoader(BaseLoader):
                 df_result[col] = df_result[col].astype('int64')
 
         return {"data_frame": df_result}
+
+    def get_data_stratified(self, state='Maharashtra', district='Mumbai', reload_data=False, ** kwargs):
+        print('fetching from athenadb...')
+        label = kwargs.pop('label', None)
+        dataframes = self.pull_dataframes_cached(reload_data=reload_data, label=label, **kwargs)
+        df = copy.copy(dataframes['case_summaries'])
+        df_bed = copy.copy(dataframes['bed_summaries'])
+        df.dropna(axis=0, how='any', inplace=True)
+        df = df.merge(df_bed, on=['state', 'district',
+                                  'date', 'partition_0'], how='outer')
+        df.rename(columns={'deaths': 'deceased', 'total cases': 'total',
+                        'active cases': 'active', 'recoveries': 'recovered'}, inplace=True)
+        df.replace(',', '', regex=True, inplace=True)
+        df = df[np.logical_and(df['state'] == state, df['district'] == district)]
+        df.loc[:, 'total':'ventilator beds occupied'] = df.loc[:,
+                                                            'total':'ventilator beds occupied']
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.infer_objects()
+        df = df[(df.select_dtypes(include='int64') > 0).sum(axis=1)
+                == len(df.select_dtypes(include='int64').columns)]
+        df.reset_index(inplace=True, drop=True)
+
+        # Column renaming and pruning
+        df = df.drop([x for x in df.columns if '_capacity' in x] +
+                    ['partition_0'], axis=1)
+        df = df.rename({'total beds occupied': 'total_beds', 'o2 beds occupied': 'o2_beds', 'icu beds occupied': 'icu',
+                        'stable symptomatic': 'symptomatic', 'stable asymptomatic': 'asymptomatic',
+                        'ventilator beds occupied': 'ventilator'}, axis='columns')
+        # New column creation
+        df['hq'] = df['active'] - df['total_beds']
+        df['non_o2_beds'] = df['total_beds'] - (df['o2_beds']+df['icu'])
+
+        # Rearranging columns
+        col = df.pop('hq')
+        df.insert(int(np.where(df.columns == 'o2_beds')[0][0]), 'hq', col)
+
+        col = df.pop('total_beds')
+        df.insert(int(np.where(df.columns == 'o2_beds')[0][0]), 'total_beds', col)
+
+        col = df.pop('non_o2_beds')
+        df.insert(int(np.where(df.columns == 'o2_beds')[0][0]), 'non_o2_beds', col)
+        date_difference = df['date'].diff()
+        date_difference.fillna(pd.Timedelta(days=2), inplace=True)
+        df = df[df['date'].diff() == pd.Timedelta(days=1)]
+        df.reset_index(inplace=True, drop=True)
+        return {"data_frame": df}
