30bce50b8713e571f151d4c98a5f1b257c17ad05
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-02-22 17:55:08 +0530

    Rename seir.py to base.py + import fix

--- models/seir/seirhd_bed.py
+++ models/seir/seirhd_bed.py
@@ -1,14 +1,9 @@
 import numpy as np
-import pandas as pd
-from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 
 from collections import OrderedDict
-import datetime
 import copy
 
-from models.seir.seir import SEIR
-from utils.fitting.ode import ODE_Solver
+from models.seir.base import SEIR
 
 class SEIRHD_Bed(SEIR):
     def __init__(self, lockdown_R0=2.2, T_inf=2.9, T_inc=5.2,
