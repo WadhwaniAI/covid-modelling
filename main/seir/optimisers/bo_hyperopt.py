e820a805dbb025a1ebea0681d9b15b4c9a85cd55
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-02-23 13:02:45 +0530

    Added _order_trials_by_loss

--- main/seir/optimisers/bo_hyperopt.py
+++ main/seir/optimisers/bo_hyperopt.py
@@ -2,6 +2,7 @@ from hyperopt import hp, tpe, fmin, Trials
 from functools import partial
 import importlib
 import numpy as np
+import copy
 
 from main.seir.optimisers import OptimiserBase
 from utils.fitting.loss import Loss_Calculator
@@ -42,6 +43,31 @@ class BO_Hyperopt(OptimiserBase):
     def init_default_params(self, df_train, default_params, train_period):
         return super().init_default_params(df_train, default_params, train_period)
 
+    def _order_trials_by_loss(self, trials_obj: Trials, sort_trials: bool = True):
+        """Orders a set of trials by their corresponding loss value
+
+        Args:
+            m_dict (dict): predictions_dict
+
+        Returns:
+            array, array: Array of params and loss values resp
+        """
+        params_array = []
+        for trial in trials_obj:
+            params_dict = copy.copy(trial['misc']['vals'])
+            for key in params_dict.keys():
+                params_dict[key] = params_dict[key][0]
+            params_array.append(params_dict)
+        params_array = np.array(params_array)
+        losses_array = np.array([trial['result']['loss'] for trial in trials_obj])
+
+        if sort_trials:
+            least_losses_indices = np.argsort(losses_array)
+            losses_array = losses_array[least_losses_indices]
+            params_array = params_array[least_losses_indices]
+        return params_array, losses_array
+
+    
     def optimise(self, df_train, default_params, variable_param_ranges, model, num_evals=3500, 
                  loss_method='rmse', loss_indices=[-20, -10], loss_compartments=['total'], 
                  loss_weights=[1], algo=tpe, seed=42, **kwargs):
@@ -90,4 +116,6 @@ class BO_Hyperopt(OptimiserBase):
                     rstate=rstate,
                     trials=trials)
         
+        params_array, losses_array = self._order_trials_by_loss(trials)
+
         return best, trials
