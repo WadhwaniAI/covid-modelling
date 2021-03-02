d7bf8ab3e200cbb5ba6d05c60a5433c211bec9ff
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-03-02 14:14:44 +0530

    Brought function up to speed with current codebase

--- data/processing/testing.py
+++ data/processing/testing.py
@@ -4,26 +4,29 @@ sys.path.append('../../')
 import pandas as pd
 import copy
 
-from data.dataloader import AthenaLoader
+import data.dataloader as dl
 
-def get_testing_data(state='Maharashtra', dist='Mumbai'):
-    dlobj = AthenaLoader()
+def get_testing_data(state, dist, dataloader='Covid19IndiaLoader'):
+    dlobj = getattr(dl, dataloader)()
     dataframes = dlobj.pull_dataframes_cached()
+    if dataloader == 'AthenaLoader':
+        df_testing = copy.copy(dataframes['testing_summary'])
+        del df_testing['partition_0']
+        del df_testing['new']
 
-    df_testing = copy.copy(dataframes['testing_summary'])
-    del df_testing['partition_0']
-    del df_testing['new']
+        df_testing = df_testing.loc[(df_testing['state'] == state.lower()) & \
+            df_testing['district'] == dist.lower(), :]
+        df_testing.dropna(axis=0, how='any', inplace=True)
+        df_testing['date'] = pd.to_datetime(df_testing['date'])
+        df_testing = df_testing.infer_objects()
+        df_testing['positives'] = df_testing['positives'].astype('int64')
+        df_testing['positives'] = df_testing['positives'].shift(-1)
+        df_testing['tests'] = df_testing['tests'].astype('int64')
+        df_testing['tpr'] = (df_testing['positives']*100/df_testing['tests'])
+        df_testing.dropna(axis=0, how='any', inplace=True)
+        df_testing.reset_index(inplace=True)
 
-    df_testing = df_testing.loc[df_testing['district'] == dist.lower(), :]
-
-    df_testing.dropna(axis=0, how='any', inplace=True)
-    df_testing['date'] = pd.to_datetime(df_testing['date'])
-    df_testing = df_testing.infer_objects()
-    df_testing['positives'] = df_testing['positives'].astype('int64')
-    df_testing['positives'] = df_testing['positives'].shift(-1)
-    df_testing['tests'] = df_testing['tests'].astype('int64')
-    df_testing['tpr'] = (df_testing['positives']*100/df_testing['tests'])
-    df_testing.dropna(axis=0, how='any', inplace=True)
-    df_testing.reset_index(inplace=True)
+    if dataloader == 'Covid19IndiaLoader':
+        df_testing = dlobj.get_data(state, dist, use_dataframe='data_all')
 
     return df_testing
