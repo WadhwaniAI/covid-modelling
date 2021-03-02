c82e5ab032d5be474851ed93f6a80ff253285c7d
nayana-wiai <nayana.bannur@wadhwaniai.org>
2021-03-03 00:34:40 +0530

    Update script

--- scripts/ihme/pipeline.py
+++ scripts/ihme/pipeline.py
@@ -5,15 +5,30 @@ import argparse
 import copy
 import datetime
 import sys
+import warnings
+import pandas as pd
 
 sys.path.append('../../')
 
 from main.ihme.fitting import single_fitting_cycle
 from utils.generic.config import read_config
 
+pd.options.mode.chained_assignment = None
+warnings.filterwarnings('ignore', module='pandas', category=RuntimeWarning)
+warnings.filterwarnings('ignore', module='curvefit', category=RuntimeWarning)
+warnings.filterwarnings('ignore', module='numpy', category=RuntimeWarning)
+
 
 def run_pipeline(config_filename):
-    config = read_config(config_filename, preprocess=False, config_dir='ihme')
+    """
+
+    Args:
+        config_filename ():
+
+    Returns:
+
+    """
+    config = read_config(config_filename, preprocess=True, config_dir='ihme')
     timestamp = datetime.datetime.now()
     output_folder = '../../misc/ihme/{}'.format(timestamp.strftime("%Y_%m%d_%H%M%S"))
     predictions_dict = single_fitting_cycle(**copy.deepcopy(config['fitting']))
