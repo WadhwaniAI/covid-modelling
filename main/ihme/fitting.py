b27beed2f55fea182f7e8df11e58bcb3b2d2cb6b
nayana-wiai <nayana.bannur@wadhwaniai.org>
2021-03-03 16:33:23 +0530

    Minor updates

--- main/ihme/fitting.py
+++ main/ihme/fitting.py
@@ -45,8 +45,15 @@ def transform_data(df, population):
     return data
 
 
+def get_covariates(df, covariates):
+    df.loc[:, 'default_cov'] = len(df) * [1.0]
+    if covariates[1] != 'default_cov':
+        pass  # TODO: Get covariate for beta
+    return df
+
+
 def data_setup(data_source, dataloading_params, smooth_jump, smooth_jump_params, split,
-               loss_compartments, rolling_average, rolling_average_params, population, **kwargs):
+               loss_compartments, rolling_average, rolling_average_params, population, covariates, **kwargs):
     """Helper function for single_fitting_cycle where data from different sources (given input) is imported
 
     Creates the following dataframes:
@@ -96,7 +103,7 @@ def data_setup(data_source, dataloading_params, smooth_jump, smooth_jump_params,
         print(smoothing['smoothing_description'])
 
     # Drop rows with NA values
-    df_district.dropna(axis=0, how='any', subset=['total'], inplace=True)
+    df_district.dropna(axis=0, how='any', subset=['total'], inplace=True)  # TODO: Replace with ycol
     df_district.reset_index(drop=True, inplace=True)
 
     # Make a copy of data without transformation or smoothing
@@ -107,7 +114,8 @@ def data_setup(data_source, dataloading_params, smooth_jump, smooth_jump_params,
 
     # Add group and covs columns
     df_district.loc[:, 'group'] = len(df_district) * [1.0]
-    df_district.loc[:, 'covs'] = len(df_district) * [1.0]
+    # df_district.loc[:, 'covs'] = len(df_district) * [1.0]
+    df_district = get_covariates(df_district, covariates)
 
     df_district.loc[:, 'day'] = (df_district['date'] - np.min(df_district['date'])).apply(lambda x: x.days)
 
@@ -295,117 +303,3 @@ def single_fitting_cycle(data, model, model_params, default_params, fitting_meth
     # record parameters for reproducibility
     predictions_dict['run_params'] = run_params
     return predictions_dict
-
-
-def create_output_folder(fname):
-    """
-    creates folder in outputs/ihme/
-
-    Args:
-        fname (str): name of folder within outputs/ihme
-
-    Returns:
-        str: output_folder path
-    """
-    output_folder = f'../../outputs/ihme/{fname}'
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    return output_folder
-
-
-# def run_cycle_compartments(dataframes, model_params, which_compartments=Columns.curve_fit_compartments(),
-#                            forecast_days=30, max_evals=1000, num_hyperopt=1, val_size=7, min_days=7, scoring='mape',
-#                            dtp=None, log=True, **config):
-#     """
-#     runs fitting cycles for all compartments in which_compartments
-#     model_params['ycol'] is ignored here
-#
-#     Args:
-#         dataframes (dict): contains smoothed and unsmoothed dataframes: train, test, df
-#         model_params (dict): model_params
-#         which_compartments (list, optional): List of compartments to fit. Defaults to Columns.curve_fit_compartments().
-#         forecast_days (int, optional): how far to predict. Defaults to 30.
-#         max_evals (int, optional): num evals in hyperparam optimisation. Defaults to 1000.
-#         num_hyperopt (int, optional): number of times to run hyperopt in parallel. Defaults to 1.
-#         val_size (int, optional): val size - hyperopt. Defaults to 7.
-#         min_days (int, optional): min train_period. Defaults to 7.
-#         scoring (str, optional): 'mape', 'rmse', or 'rmsle. Defaults to 'mape'.
-#         dtp ([type], optional): district total population. Defaults to None.
-#         log (bool, optional): whether to fit to log(rate). Defaults to True.
-#
-#     Returns:
-#         dict: results_dict
-#     """
-#     xform_func = lograte_to_cumulative if log else rate_to_cumulative
-#     compartment_names = [col.name for col in which_compartments]
-#     results = {}
-#     ycols = {col: '{log}{colname}_rate'.format(log='log_' if log else '', colname=col.name) for col in
-#              which_compartments}
-#     loss_dict = dict()
-#     for i, col in enumerate(which_compartments):
-#         col_params = copy.copy(model_params)
-#         if config.get("active_log_derf", False) and col.name == 'hospitalised':
-#             col_params['func'] = functions.log_derf
-#         col_params['ycol'] = ycols[col]
-#         results[col.name] = run_cycle(
-#             dataframes, col_params, dtp=dtp, xform_func=xform_func,
-#             max_evals=max_evals, num_hyperopt=num_hyperopt, val_size=val_size,
-#             min_days=min_days, scoring=scoring, log=log, forecast_days=forecast_days,
-#             **config)
-#
-#         # Aggregate Results
-#         pred = results[col.name]['df_prediction'].set_index('date')
-#         loss_dict[col.name] = results[col.name]['df_loss']
-#         if i == 0:
-#             predictions = pd.DataFrame(index=pred.index, columns=compartment_names + list(ycols.values()), dtype=float)
-#
-#         predictions.loc[pred.index, col.name] = xform_func(pred[ycols[col]], dtp)
-#         predictions.loc[pred.index, ycols[col]] = pred[ycols[col]]
-#
-#     df_loss = pd.concat(loss_dict.values(), axis=0, keys=compartment_names,
-#                         names=['compartment', 'split', 'loss_function'])
-#     df_loss.name = 'loss'
-#     df_train_loss_pointwise = pd.concat([results[comp]['df_train_loss_pointwise'] for comp in compartment_names],
-#                                         keys=compartment_names, names=['compartment', 'split', 'loss_function'])
-#     df_test_loss_pointwise = pd.concat([results[comp]['df_test_loss_pointwise'] for comp in compartment_names],
-#                                        keys=compartment_names, names=['compartment', 'split', 'loss_function'])
-#
-#     predictions.reset_index(inplace=True)
-#     df_train = dataframes['train'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#     df_val = dataframes['test'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#     df_district = dataframes['df'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#     df_train_nora = dataframes['train_nora'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#     df_val_nora = dataframes['test_nora'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#     df_district_nora = dataframes['df_nora'][compartment_names + list(ycols.values()) + [model_params['date']]]
-#
-#     draws = None
-#     if model_params['pipeline_args']['n_draws'] > 0:
-#         draws = {
-#             col.name: {
-#                 'draws': xform_func(results[col.name]['draws'], dtp),
-#                 'no_xform_draws': results[col.name]['draws'],
-#             } for col in which_compartments
-#         }
-#
-#     final = {
-#         'best_params': {col.name: results[col.name]['best_params'] for col in which_compartments},
-#         'variable_param_ranges': model_params['priors']['fe_bounds'],
-#         'n_days': {col.name: results[col.name]['n_days'] for col in which_compartments},
-#         'df_prediction': predictions,
-#         'df_district': df_district,
-#         'df_train': df_train,
-#         'df_val': df_val,
-#         'df_district_nora': df_district_nora,
-#         'df_train_nora': df_train_nora,
-#         'df_val_nora': df_val_nora,
-#         'df_loss': df_loss,
-#         'df_train_loss_pointwise': df_train_loss_pointwise,
-#         'df_test_loss_pointwise': df_test_loss_pointwise,
-#         'data_last_date': df_district[model_params['date']].max(),
-#         'draws': draws,
-#         'mod.params': {col.name: results[col.name]['mod.params'] for col in which_compartments},
-#         'individual_results': results,
-#         'district_total_pop': results[which_compartments[0].name]['district_total_pop'],
-#     }
-#
-#     return final
