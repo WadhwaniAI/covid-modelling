3b43a032ead200f9d9dc41ee99c1d099f73260eb
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-03-03 18:21:22 +0530

    Added docstrings for ny times

--- data/dataloader/ny_times.py
+++ data/dataloader/ny_times.py
@@ -4,14 +4,26 @@ from data.dataloader.base import BaseLoader
 
 
 class NYTLoader(BaseLoader):
+    """Dataloader that time series case data for US states, counties, and US from
+        the NY Times github repo 'https://www.github.com/nytimes/covid-19-data/'
+
+        Allows the user to do fitting on US states, US counties
+
+    Args:
+        BaseLoader (abstract class): Abstract Data Loader Class
+    """
     def __init__(self):
         super().__init__()
 
     # Loads time series case data for US counties and states from the New York Times github repo
     def pull_dataframes(self, **kwargs):
         """
-        This function parses the us-states and us-counties CSVs on NY Times's github repo and converts them to pandas dataframes
+        This function parses the us-states and us-counties CSVs on
+        NY Times's github repo and converts them to pandas dataframes
         Returns dict of dataframes for states and counties
+
+        Returns:
+            dict{str : pd.DataFrame}: dict of pulled dataframes
         """
         dataframes = dict()
         df_counties = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
@@ -26,6 +38,18 @@ class NYTLoader(BaseLoader):
         return super().pull_dataframes_cached(reload_data=reload_data, label=label, **kwargs)
 
     def get_data(self, state, county=None, reload_data=False, **kwargs):
+        """Main function serving as handshake between data and fitting modules
+
+        Args:
+            state (str): The US state of the region of interest
+            county (str, optional): The county of the region of interest. 
+            If None, fitting done on given state. Defaults to None.
+            reload_data (bool, optional): arg for pull_dataframes_cached. If true, data is 
+            pulled afresh, rather than using the cache. Defaults to False.
+
+        Returns:
+            dict: dict with singular element containing the processed dataframe
+        """
         dataframes = self.pull_dataframes_cached(reload_data=reload_data, **kwargs)
         if county is not None:
             df = dataframes['counties']
