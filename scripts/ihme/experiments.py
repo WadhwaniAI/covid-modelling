e48d8c7360905dc5257521a71409ddb5151c55ac
nayana-wiai <nayana.bannur@wadhwaniai.org>
2021-03-09 00:40:22 +0530

    Update script

--- scripts/ihme/experiments.py
+++ scripts/ihme/experiments.py
@@ -8,24 +8,24 @@ Run as: python3 -W ignore experiments.py -b region.yaml -d driver.yaml
 import argparse
 import copy
 import datetime
+import itertools
 import json
 import logging
+import multiprocessing
 import os
 import pickle
 import sys
-from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
 from joblib import Parallel, delayed
 from tqdm import tqdm
-import multiprocessing
 
 sys.path.append('../../')
 
 from main.ihme.fitting import single_fitting_cycle
 from utils.fitting.util import update_dict, chunked, CustomEncoder
-from utils.generic.config import read_config, process_config_ihme, make_date_str, generate_combinations, generate_config
+from utils.generic.config import read_config, process_config_ihme, make_date_str, generate_configs_from_driver
 
 default_loss_methods = ['mape', 'rmse', 'rmse_log']
 
@@ -56,30 +56,27 @@ def create_output(predictions_dict, output_folder, tag):
         json.dump(make_date_str(predictions_dict['config']), f, indent=4, cls=CustomEncoder)
 
 
-def get_experiments(driver_config_filename, base_config_filename):
+def get_experiment(driver_config_filename):
     """Get experiment configuration"""
     logging.info('Getting experiment choices')
-    experiments = read_config(driver_config_filename, preprocess=False, config_dir='other')
-    label = base_config_filename.split('.')[0]
-    experiments = generate_config(experiments)
-    experiments = generate_combinations(experiments)
-    configs = {f'{label}/{i}': exp for i, exp in enumerate(experiments)}
+    configs = generate_configs_from_driver(driver_config_filename)
     return configs
 
 
 def run(config):
     """Run single experiment for given config"""
     predictions_dict = single_fitting_cycle(**copy.deepcopy(config['fitting']))
+    predictions_dict['fitting_date'] = datetime.datetime.now().strftime("%Y-%m-%d")
     return predictions_dict
 
 
-def run_parallel(run_name, params, base_config_filename):
+def run_parallel(key, params):
     """Read config and run corresponding experiment"""
-    config = read_config(base_config_filename, preprocess=False, config_dir='ihme')
+    config = read_config(f'{key}.yaml', preprocess=False, config_dir='ihme')
     config = update_dict(config, params)
     config = process_config_ihme(config)
     try:
-        logging.info(f'Start run: {run_name}')
+        logging.info(f'Start run: {key}')
         x = run(config)
         x['config'] = config
         plt.close('all')
@@ -89,8 +86,7 @@ def run_parallel(run_name, params, base_config_filename):
     return x
 
 
-def perform_batch_runs(base_config_filename='default.yaml', driver_config_filename='list_of_exp.yaml',
-                       output_folder=None):
+def perform_batch_runs(driver_config_filename='list_of_exp.yaml', output_folder=None):
     """Run all experiments"""
     # Specifying the folder where checkpoints will be saved
     timestamp = datetime.datetime.now().strftime("%Y_%m%d_%H%M%S")
@@ -99,30 +95,29 @@ def perform_batch_runs(base_config_filename='default.yaml', driver_config_filena
     os.makedirs(output_folder, exist_ok=True)
     n_jobs = multiprocessing.cpu_count()
 
-    # Get experiment choices
-    what_to_vary = get_experiments(driver_config_filename, base_config_filename)
+    # Get generator of partial configs corresponding to experiments
+    what_to_vary = get_experiment(driver_config_filename)
 
     # Run experiments
-    for i, chunk in enumerate(chunked(what_to_vary.items(), n_jobs)):
+    logging.info('Start batch runs')
+    for i, chunk in enumerate(chunked(what_to_vary, n_jobs)):
+        chunk1, chunk2 = itertools.tee(chunk, 2)
         print(f'Group {i}')
-        partial_run_parallel = partial(run_parallel, base_config_filename=base_config_filename)
-        logging.info('Start batch runs')
         predictions_arr = Parallel(n_jobs=n_jobs)(
-            delayed(partial_run_parallel)(key, config) for key, config in tqdm(chunk.items()))
+            delayed(run_parallel)(key, config) for key, config in tqdm(chunk1))
 
-        for j, key in tqdm(enumerate(chunk.keys())):
+        # Save results
+        for j, (key, _) in tqdm(enumerate(chunk2)):
             if isinstance(predictions_arr[j], dict):
-                create_output(predictions_arr[j], output_folder, key)
-                # with open(f'{output_folder}/{key}/predictions_dict.pkl', 'wb') as f:
+                create_output(predictions_arr[j], output_folder, f'{key}/{n_jobs * i + j}')
+                # with open(f'{output_folder}/{key}_predictions_dict.pkl', 'wb') as f:
                 #     pickle.dump(predictions_arr[j], f)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR)
     parser = argparse.ArgumentParser(description="IHME Batch Running Script")
-    parser.add_argument("-b", "--base_config", type=str, required=True,
-                        help="base config to use while running the script")
-    parser.add_argument("-d", "--driver_config", type=str, required=False, default='list_of_exp.yaml',
+    parser.add_argument("-d", "--driver_config", type=str, required=True,
                         help="driver config used for multiple experiments")
     parsed_args = parser.parse_args()
-    perform_batch_runs(parsed_args.base_config, parsed_args.driver_config)
+    perform_batch_runs(parsed_args.driver_config)
