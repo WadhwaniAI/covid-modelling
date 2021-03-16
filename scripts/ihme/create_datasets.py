b46d28cd5857e5d45f97c1efa6c880eb09894f46
nayana-wiai <nayana.bannur@wadhwaniai.org>
2021-03-16 17:36:26 +0530

    Update script

--- scripts/ihme/create_datasets.py
+++ scripts/ihme/create_datasets.py
@@ -14,7 +14,7 @@ sys.path.append('../../')
 from data.processing import get_data
 
 
-def create_outputs(input_path, output_path):
+def create_outputs(input_path, output_path, synthetic_days):
     folders = [f.name for f in os.scandir(input_path) if f.is_dir()]
     for folder in folders:
         folder_path = os.path.join(input_path, folder)
@@ -35,17 +35,25 @@ def create_outputs(input_path, output_path):
             test_period = config['fitting']['split']['test_period']
             end_date = start_date + datetime.timedelta(train_period + val_period +
                                                        test_period)
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
 
         # Get IHME predictions
         predictions = pd.read_csv(os.path.join(folder_path, 'df_prediction.csv'))
+        predictions['date'] = pd.to_datetime(predictions['date'])
 
         # Combine datasets
         actual_data = original_data['data_frame']
-        actual_data = actual_data[actual_data['date'] <= end_date]
-        synthetic_data = predictions[predictions['date'] > end_date]
-        dataset = pd.concat(actual_data, synthetic_data)
+        actual_data_before = actual_data[actual_data['date'] <= end_date]
+        actual_data_after = actual_data[actual_data['date'] > end_date +
+                                        datetime.timedelta(synthetic_days)]
+        synthetic_data = predictions[predictions['date'] > end_date].reset_index().iloc[
+                         :synthetic_days, :]
+        synthetic_data = synthetic_data[
+            ['date'] + config['fitting']['loss']['loss_compartments']]
+        dataset = pd.concat([actual_data_before, synthetic_data, actual_data_after])
+        dataset.reset_index(inplace=True)
 
-        output_folder = os.path.join(output_path, end_date.strftime('%Y-%m-%d'))
+        output_folder = os.path.join(output_path, (end_date + datetime.timedelta(synthetic_days)).strftime('%Y-%m-%d'))
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
 
@@ -56,6 +64,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_path", help="input folder path", required=True)
     parser.add_argument("-o", "--output_path", help="output folder path", required=True)
+    parser.add_argument("-n", "--synthetic_days", help="number of days of synthetic data used", type=int, required=True)
     args = parser.parse_args()
 
-    create_outputs(args.input_path, args.output_path)
+    create_outputs(args.input_path, args.output_path, args.synthetic_days)
