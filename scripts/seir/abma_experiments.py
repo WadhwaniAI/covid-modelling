7679dcbe88e4d7f4798a7aa644e7967fa3d75663
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-03-16 17:39:31 +0530

    Compatible with opensourcing

--- scripts/seir/abma_experiments.py
+++ scripts/seir/abma_experiments.py
@@ -3,6 +3,7 @@ experiments.py
 Run hyperparameter tuning and other experiments for variants of compartmental models using the ABMA approach.
 """
 import argparse
+import datetime
 import itertools
 import json
 import logging
@@ -10,7 +11,6 @@ import multiprocessing
 import os
 import pickle
 import sys
-import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,11 +18,16 @@ import yaml
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
-sys.path.append('../../')
-
 from scripts.seir.common import *
-from utils.fitting.util import update_dict, chunked, CustomEncoder
-from utils.generic.config import read_config, process_config_seir, make_date_str, generate_configs_from_driver
+from utils.fitting.util import CustomEncoder, chunked, update_dict
+from utils.generic.config import (
+    generate_configs_from_driver,
+    make_date_str,
+    process_config_seir,
+    read_config,
+)
+
+sys.path.append('../../')
 
 
 def create_output(predictions_dict, output_folder, tag):
@@ -38,25 +43,26 @@ def create_output(predictions_dict, output_folder, tag):
     for inner in ['df_prediction', 'df_district', 'df_train', 'df_val', 'df_loss', 'df_district_unsmoothed']:
         if inner in predictions_dict and predictions_dict[inner] is not None:
             predictions_dict[inner].to_csv(f'{directory}/{inner}.csv')
-    for inner in ['trials', 'run_params', 'optimiser', 'plots', 'smoothing_description', 'default_params']:
+    for inner in ['trials', 'run_params', 'plots', 'smoothing_description', 'default_params']:
         with open(f'{directory}/{inner}.pkl', 'wb') as f:
             pickle.dump(predictions_dict[inner], f)
     if 'ensemble_mean' in predictions_dict['forecasts']:
         predictions_dict['forecasts']['ensemble_mean'].to_csv(
             f'{directory}/ensemble_mean_forecast.csv')
-    predictions_dict['trials_processed']['predictions'][0].to_csv(
-        f'{directory}/trials_processed_predictions.csv')
-    np.save(f'{directory}/trials_processed_params.npy',
-            predictions_dict['trials_processed']['params'])
-    np.save(f'{directory}/trials_processed_losses.npy',
-            predictions_dict['trials_processed']['losses'])
+    predictions_dict['trials']['predictions'][0].to_csv(
+        f'{directory}/trials_predictions.csv')
+    np.save(f'{directory}/trials_params.npy',
+            predictions_dict['trials']['params'])
+    np.save(f'{directory}/trials_losses.npy',
+            predictions_dict['trials']['losses'])
     d[f'data_last_date'] = predictions_dict['data_last_date']
     d['fitting_date'] = predictions_dict['fitting_date']
-    np.save(f'{directory}/m2_beta.npy', predictions_dict['m2']['beta'])
+    np.save(f'{directory}/beta.npy', predictions_dict['beta'])
     with open(f'{directory}/other.json', 'w') as f:
         json.dump(d, f, indent=4)
     with open(f'{directory}/config.json', 'w') as f:
-        json.dump(make_date_str(predictions_dict['config']), f, indent=4, cls=CustomEncoder)
+        json.dump(make_date_str(
+            predictions_dict['config']), f, indent=4, cls=CustomEncoder)
     with open(f'{directory}/config.yaml', 'w') as f:
         yaml.dump(make_date_str(predictions_dict['config']), f)
 
@@ -70,9 +76,9 @@ def get_experiment(driver_config_filename):
 
 def run(config):
     """Run single experiment for given config"""
-    predictions_dict = (config)
-    predictions_dict['m1']['forecasts'] = {}
-    predictions_dict['m2']['forecasts'] = {}
+    predictions_dict = fitting(config)
+    predictions_dict['forecasts'] = {}
+    predictions_dict['forecasts'] = {}
     uncertainty = fit_beta(predictions_dict, config)
     process_ensemble(predictions_dict, uncertainty)
     plot_ensemble_forecasts(predictions_dict, config)
@@ -85,25 +91,26 @@ def run_parallel(key, params):
     config = update_dict(config, params)
     config_copy = copy.deepcopy(config)
     config = process_config_seir(config)
-    try:
-        logging.info(f'Start run: {key}')
-        x = run(config)
-        x['config'] = config_copy
-        plt.close('all')
-    except Exception as e:
-        x = e
-        logging.error(e)
+    # try:
+    logging.info(f'Start run: {key}')
+    x = run(config)
+    x['config'] = config_copy
+    plt.close('all')
+    # except Exception as e:
+    #     x = e
+    #     logging.error(e)
     return x
 
 
-def perform_batch_runs(driver_config_filename='list_of_exp.yaml', output_folder=None):
+def perform_batch_runs(driver_config_filename='list_of_exp.yaml', output_folder=None, n_jobs=None):
     """Run all experiments"""
     # Specifying the folder where checkpoints will be saved
     timestamp = datetime.datetime.now().strftime("%Y_%m%d_%H%M%S")
     if output_folder is None:
         output_folder = f'../../outputs/seir/{timestamp}'
     os.makedirs(output_folder, exist_ok=True)
-    n_jobs = multiprocessing.cpu_count()
+    if n_jobs is None:
+        n_jobs = multiprocessing.cpu_count()
 
     # Get generator of partial configs corresponding to experiments
     what_to_vary = get_experiment(driver_config_filename)
@@ -119,7 +126,8 @@ def perform_batch_runs(driver_config_filename='list_of_exp.yaml', output_folder=
         # Save results
         for j, (key, _) in tqdm(enumerate(chunk2)):
             if isinstance(predictions_arr[j], dict):
-                create_output(predictions_arr[j], output_folder, f'{key}/{n_jobs * i + j}')
+                create_output(predictions_arr[j],
+                              output_folder, f'{key}/{n_jobs * i + j}')
                 # with open(f'{output_folder}/{key}_predictions_dict.pkl', 'wb') as f:
                 #     pickle.dump(predictions_arr[j], f)
 
@@ -129,5 +137,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="SEIR ABMA Batch Running Script")
     parser.add_argument("-d", "--driver_config", type=str, required=True,
                         help="driver config used for multiple experiments")
+    parser.add_argument("-o", "--output_folder", type=str, required=True,
+                        help="Where to save the outputs")
+    parser.add_argument("--n_jobs", type=int, help="number of threads")
     parsed_args = parser.parse_args()
-    perform_batch_runs(parsed_args.driver_config)
+    perform_batch_runs(parsed_args.driver_config,
+                       parsed_args.output_folder, parsed_args.n_jobs)
