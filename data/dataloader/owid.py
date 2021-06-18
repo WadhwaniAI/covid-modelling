132ae0a5a85e1a862b87a7966206c604cf4ad44e
Sansiddh Jain <sansiddh@wadhwaniai.org>
2021-06-18 13:49:33 +0530

    OWID dataloader

---
+++ data/dataloader/owid.py
@@ -0,0 +1,27 @@
+from .base import BaseLoader
+import pandas as pd
+
+class OWIDLoader(BaseLoader):
+    def __init__(self):
+        super().__init__()
+
+    def pull_dataframes(self, **kwargs):
+        """
+        This function parses the us-states and us-counties CSVs on
+        NY Times's github repo and converts them to pandas dataframes
+        Returns dict of dataframes for states and counties
+
+        Returns:
+            dict{str : pd.DataFrame}: dict of pulled dataframes
+        """
+        dataframes = dict()
+        df_counties = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',
+                                  error_bad_lines=False)
+        df_states = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv',
+                                error_bad_lines=False)
+        dataframes['counties'] = df_counties
+        dataframes['states'] = df_states
+        return dataframes
+
+    def pull_dataframes_cached(self, reload_data=False, label=None, **kwargs):
+        return super().pull_dataframes_cached(reload_data=reload_data, label=label, **kwargs)
